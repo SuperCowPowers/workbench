561f5e731274328e64869f7f9b7bee050d7a5c27
Brian Wylie <briford.wylie@gmail.com>
2023-02-23 15:37:56 -0700

    adding some scripts to introspect SageWorks data sources

---
+++ scripts/data_source_tags.py
@@ -0,0 +1,40 @@
+"""Show a list of files/tables and tags from the given AWS catalog database"""
+import sys
+import argparse
+from os.path import basename
+
+# Local imports
+from glue_utils.glue_meta import GlueMeta
+
+
+# Class: List Tags
+class ListTags:
+    """ListTags: """
+    def __init__(self, catalog_database: str):
+        # Our Glue Meta Class takes care of the low level details
+        self.glue_meta = GlueMeta(catalog_database, skip_ignore=False)
+
+    def list_tags(self):
+        """List all the files/tags for the tables in this AWS catalog database"""
+        catalog_table_data = self.glue_meta.get_table_data()
+        for name, table_info in catalog_table_data.items():
+            file_name = basename(table_info['location'])
+            print(f"{file_name}   Tags: {table_info['tags']}...")
+            # print(f"File: {table_info['location']}...")
+
+
+if __name__ == '__main__':
+
+    # Collect args from the command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--database', type=str, default='sageworks', help='The AWS catalog database name')
+    args, commands = parser.parse_known_args()
+
+    # Check for unknown args
+    if commands:
+        print('Unrecognized args: %s' % commands)
+        sys.exit(1)
+
+    # Create our Class and List the Tags
+    list_tags = ListTags(args.database)
+    list_tags.list_tags()
---
+++ scripts/list_data_sources.py
@@ -0,0 +1,39 @@
+"""Show a list of the SageWorks data sources in the AWS catalog database"""
+import sys
+import argparse
+
+# Local Imports
+from sageworks.utils.glue_meta import GlueMeta
+
+
+# Class: List Tags
+class ListTags:
+    """ListTags: """
+    def __init__(self, catalog_database: str):
+        # Our Glue Meta Class takes care of the low level details
+        self.glue_meta = GlueMeta(catalog_database, skip_ignore=False)
+
+    def list_tags(self):
+        """List all the files/tags for the tables in this AWS catalog database"""
+        catalog_table_data = self.glue_meta.get_table_data()
+        for name, table_info in catalog_table_data.items():
+            print(f"\n*** {name} ***")
+            for key, value in table_info.items():
+                print(f"\t{key}: {value}")
+
+
+if __name__ == '__main__':
+
+    # Collect args from the command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--database', type=str, default='sageworks', help='The AWS catalog database name')
+    args, commands = parser.parse_known_args()
+
+    # Check for unknown args
+    if commands:
+        print('Unrecognized args: %s' % commands)
+        sys.exit(1)
+
+    # Create our Class and List the Tags
+    list_tags = ListTags(args.database)
+    list_tags.list_tags()
