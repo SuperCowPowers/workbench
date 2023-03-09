974198eb48d687724480fe975dc1ef23b89b18c0
Brian Wylie <briford.wylie@gmail.com>
2023-03-09 13:32:47 -0700

    adding some random pandas helpers

---
+++ src/sageworks/transforms/transform_utils/pandas_helpers.py
@@ -0,0 +1,30 @@
+"""Helper methods to show info about the dataframe"""
+import pandas as pd
+
+
+def get_percent_null(df):
+    print('Dataframe ({:d} rows)'.format(len(df)))
+    s = df.isna().mean().round(3) * 100.0
+    s.name = '%Null'
+    return s
+
+
+def unique(df):
+    s = df.nunique()
+    s.name = '#unique'
+    return s
+
+
+def examples(df):
+    first_n = [df[c].unique()[:10].tolist() for c in df.columns]
+    s = pd.Series(first_n, df.columns)
+    s.name = 'Examples'
+    return s
+
+
+def info(df):
+    s1 = get_percent_null(df)
+    s2 = unique(df)
+    s3 = examples(df)
+    df = pd.concat([s1, s2, s3], axis=1)
+    return df.sort_values('%Null')
