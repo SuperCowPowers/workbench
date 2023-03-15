deb373a9864c61fda6e9539febef121096dd55f6
Brian Wylie <briford.wylie@gmail.com>
2023-03-15 12:30:51 -0600

    pushing the dataframe conversions into the light_transform parent class

--- src/sageworks/transforms/data_to_data/light/clean_data.py
+++ src/sageworks/transforms/data_to_data/light/clean_data.py
@@ -1,24 +1,18 @@
 """CleanData: Example Class that demonstrates data cleanup for Light DataSources using Pandas"""
 
 # Local imports
-from sageworks.transforms.transform import Transform, TransformInput, TransformOutput
-from sageworks.transforms.pandas_transforms.data_to_pandas import DataToPandas
-from sageworks.transforms.pandas_transforms.pandas_to_data import PandasToData
+from sageworks.transforms.data_to_data.light.light_transform import LightTransform
 
 
-class CleanData(Transform):
+class CleanData(LightTransform):
     def __init__(self, input_uuid: str = None, output_uuid: str = None):
         """CleanData: Class for filtering, sub-setting, and value constraints on Light DataSources uses Pandas"""
 
         # Call superclass init
         super().__init__(input_uuid, output_uuid)
 
-        # Set up all my instance attributes
-        self.input_type = TransformInput.DATA_SOURCE
-        self.output_type = TransformOutput.DATA_SOURCE
-
-    def transform(self):
-        """Pull the input DataSource, clean the data, and output to a DataSource"""
+    def _transform(self, dropna='any'):
+        """Simple Clean Data, will improve later"""
 
         """
         Notes for later:
@@ -35,27 +29,19 @@ class CleanData(Transform):
             - Impute the value (using inference/context to fill in a value)
         """
 
-        # Grab the Input (Data Source)
-        input_df = DataToPandas(self.input_uuid).get_output()  # Shorthand for transform, get_output
+        # Drop Rows that have NaNs in them
+        orig_rows = len(self.input_df)
+        self.output_df = self.input_df.dropna(axis=0, how=dropna)
+        if len(self.output_df) != orig_rows:
+            self.log.info(f"Dropping {orig_rows - len(self.output_df)} rows that have a NaN in them")
 
-        # Drop Rows that have ANY NaNs in them
-        orig_rows = len(input_df)
-        input_df = input_df.dropna(axis=0, how='any')
-        if len(input_df) != orig_rows:
-            self.log.info(f"Dropping {orig_rows - len(input_df)} rows that have a NaN in them")
-
-        # Drop Columns that have ANY NaNs in them
-        orig_columns = input_df.columns.tolist()
-        input_df = input_df.dropna(axis=1, how='any')
-        remaining_columns = input_df.columns.tolist()
+        # Drop Columns that have NaNs in them
+        orig_columns = self.output_df.columns.tolist()
+        self.output_df = self.output_df.dropna(axis=1, how=dropna)
+        remaining_columns = self.output_df.columns.tolist()
         if remaining_columns != orig_columns:
             dropped_columns = list(set(remaining_columns).difference(set(orig_columns)))
             self.log.info(f"Dropping {dropped_columns} columns that have a NaN in them")
-
-        # Now publish to the output location
-        output_data = PandasToData(self.output_uuid)
-        output_data.set_input(input_df)
-        output_data.transform()
 
 
 # Simple test of the CleanData functionality
@@ -67,7 +53,7 @@ def test():
     # Create the class with inputs and outputs and invoke the transform
     input_uuid = 'aqsol_data'
     output_uuid = 'aqsol_data_clean'
-    CleanData(input_uuid, output_uuid).transform()
+    CleanData(input_uuid, output_uuid).transform(dropna='any')
 
     # Grab the output and query it for a dataframe
     output = AthenaSource(output_uuid)
