f42334eabb4b396134b6243cf9adf2668fedd050
Brian Wylie <briford.wylie@gmail.com>
2023-03-21 15:59:26 -0600

    adjusting padding and margins

--- applications/artifact_viewer/layout.py
+++ applications/artifact_viewer/layout.py
@@ -6,18 +6,18 @@ import dash_bootstrap_components as dbc
 def artifact_layout(app: Dash, components: dict) -> html.Div:
     # Just put all the tables in as Rows for Now (do something fancy later)
     layout = html.Div(children=[
-        dbc.Row(html.H2(app.title)),
-        dbc.Row(html.Div('Last Updated: ', id='last-updated',
-                         style={'color': 'rgb(200, 200, 200)', 'fontSize': 14, 'padding': '0px 0px 0px 100px'})),
-        dbc.Row(html.H3('Incoming Data'), style={'padding': '50px 0px 0px 0px'}),
+        dbc.Row([html.H2(app.title), html.Div('Last Updated: ', id='last-updated',
+                                             style={'color': 'rgb(200, 200, 200)',
+                                                    'fontSize': 14, 'padding': '0px 0px 0px 100px'})]),
+        dbc.Row(html.H3('Incoming Data'), style={'padding': '20px 0px 0px 0px'}),
         dbc.Row(components['incoming_data']),
-        dbc.Row(html.H3('Data Sources'), style={'padding': '50px 0px 0px 0px'}),
+        dbc.Row(html.H3('Data Sources'), style={'padding': '20px 0px 0px 0px'}),
         dbc.Row(components['data_sources']),
-        dbc.Row(html.H3('Feature Sets'), style={'padding': '50px 0px 0px 0px'}),
+        dbc.Row(html.H3('Feature Sets'), style={'padding': '20px 0px 0px 0px'}),
         dbc.Row(components['feature_sets']),
-        dbc.Row(html.H3('Models'), style={'padding': '50px 0px 0px 0px'}),
+        dbc.Row(html.H3('Models'), style={'padding': '20px 0px 0px 0px'}),
         dbc.Row(components['models']),
-        dbc.Row(html.H3('Endpoints'), style={'padding': '50px 0px 0px 0px'}),
+        dbc.Row(html.H3('Endpoints'), style={'padding': '20px 0px 0px 0px'}),
         dbc.Row(components['endpoints']),
         dcc.Interval(id='interval-component', interval=5000, n_intervals=0)
         ], style={'margin': '30px'})
