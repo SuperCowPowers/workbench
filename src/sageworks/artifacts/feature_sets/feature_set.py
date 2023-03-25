efc55d87b980c11820670833d620262d674a75a2
Brian Wylie <briford.wylie@gmail.com>
2023-03-25 12:12:02 -0600

    improving the training data to S3 generation code

--- src/sageworks/artifacts/feature_sets/feature_set.py
+++ src/sageworks/artifacts/feature_sets/feature_set.py
@@ -24,6 +24,8 @@ class FeatureSet(AthenaSource):
         self.feature_set_name = feature_set_name
         self.aws_meta = AWSServiceBroker()
         self.feature_meta = self.aws_meta.get_metadata(ServiceCategory.FEATURE_STORE).get(self.feature_set_name)
+        self.record_id = self.feature_meta['RecordIdentifierFeatureName']
+        self.event_time = self.feature_meta['EventTimeFeatureName']
 
         # Pull Athena and S3 Storage information from metadata
         self.athena_database = self.feature_meta['sageworks'].get('athena_database')
@@ -57,21 +59,49 @@ class FeatureSet(AthenaSource):
         """
         return self.feature_store
 
-    def create_s3_training_data(self) -> str:
+    def create_s3_training_data(self, training_split=80, test_split=20, val_split=0) -> str:
         """Create some Training Data (S3 CSV) from a Feature Set using standard options. If you want
            additional options/features use the get_feature_store() method and see AWS docs for all
            the details: https://docs.aws.amazon.com/sagemaker/latest/dg/feature-store-create-a-dataset.html
+           Args:
+               training_split (int): Percentage of data that goes into the TRAINING set
+               test_split (int): Percentage of data that goes into the TEST set
+               val_split (int): Percentage of data that goes into the VALIDATION set (default=0)
            Returns:
                str: The full path/file for the CSV file created by Feature Store create_dataset()
         """
+
+        # Set up the S3 Query results path
         date_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H:%M:%S")
         s3_output_path = self.feature_sets_s3_path + f"/{self.feature_set_name}/datasets/all_{date_time}"
-        my_feature_group = FeatureGroup(name=self.feature_set_name, sagemaker_session=self.sm_session)
-        s3_output_file, query = self.feature_store.create_dataset(
-            base=my_feature_group,
-            output_path=s3_output_path
-        ).to_csv_file()
-        return s3_output_file
+
+        # Get the snapshot query
+        query = self.snapshot_query()
+
+        # Make the query
+        athena_query = FeatureGroup(name=self.feature_set_name, sagemaker_session=self.sm_session).athena_query()
+        athena_query.run(query, output_location=s3_output_path)
+        self.log.info('Waiting for Athena Query...')
+        athena_query.wait()
+        query_execution = athena_query.get_query_execution()
+
+        # Get the full path to the S3 files with the results
+        full_s3_path = s3_output_path + f"/{query_execution['QueryExecution']['QueryExecutionId']}.csv"
+        return full_s3_path
+
+    def snapshot_query(self):
+        """An Athena query to get the latest snapshot of features"""
+        # Remove FeatureGroup metadata columns that might have gotten added
+        columns = self.column_names()
+        filter_columns = ['write_time', 'api_invocation_time', 'is_deleted']
+        columns = ', '.join([x for x in columns if x not in filter_columns])
+
+        query = (f'SELECT {columns} '
+                 f'    FROM (SELECT *, row_number() OVER (PARTITION BY {self.record_id} '
+                 f'        ORDER BY {self.event_time} desc, api_invocation_time DESC, write_time DESC) AS row_num '
+                 f'        FROM "{self.athena_table}") '
+                 '    WHERE row_num = 1 and  NOT is_deleted;')
+        return query
 
     def delete(self):
         """Delete the Feature Set: Feature Group, Catalog Table, and S3 Storage Objects"""
@@ -106,8 +136,8 @@ def test():
 
     # Call the various methods
 
-    # Lets do a check/validation of the feature set
-    assert(my_features.check())
+    # Let's do a check/validation of the feature set
+    assert (my_features.check())
 
     # How many rows and columns?
     num_rows = my_features.num_rows()
