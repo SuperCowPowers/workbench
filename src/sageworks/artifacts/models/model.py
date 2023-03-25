4675111a1f64a051fa5a49ec10f18f8eb6727e8b
Brian Wylie <briford.wylie@gmail.com>
2023-03-25 13:30:50 -0600

    adding in the features to model transform with a light model harness/template

---
+++ src/sageworks/artifacts/models/model.py
@@ -0,0 +1,129 @@
+"""Model: SageWorks Model accessible through Athena"""
+import json
+from datetime import datetime
+
+
+# SageWorks Imports
+from sageworks.artifacts.artifact import Artifact
+from sageworks.aws_service_broker.aws_service_broker import ServiceCategory, AWSServiceBroker
+from sageworks.aws_service_broker.aws_sageworks_role_manager import AWSSageWorksRoleManager
+
+
+class Model(Artifact):
+
+    def __init__(self, model_name):
+        """Model: SageWorks Model accessible through Athena
+
+        Args:
+            model_name (str): Name of Model in SageWorks Metadata.
+        """
+        # Call SuperClass Initialization
+        super().__init__()
+
+        # Grab an AWS Metadata Broker object and pull information for Models
+        self.model_name = model_name
+        self.aws_meta = AWSServiceBroker()
+        self.model_meta = self.aws_meta.get_metadata(ServiceCategory.MODELS).get(self.model_name)
+        self.latest_model = self.model_meta[0]
+        self.model_package_arn = self.latest_model['ModelPackageDetails']['ModelPackageArn']
+
+        # Pull Model Package Description Data
+        try:
+            self.model_info = json.loads(self.latest_model['ModelPackageDescription'])
+            self.input = self.model_info['input']
+            self.description = self.model_info['info']
+            self.model_tags = self.model_info['tags']
+        except (json.JSONDecoder, KeyError):
+            self.model_info = self.latest_model['ModelPackageDescription']
+            self.input = '-'
+            self.description = self.model_info
+            self.model_tags = '-'
+
+        # Grab our SageMaker Session
+        self.sm_session = AWSSageWorksRoleManager().sagemaker_session()
+
+        # All done
+        self.log.info(f"Model Initialized: {model_name}")
+
+    def check(self) -> bool:
+        """Does the feature_set_name exist in the AWS Metadata?"""
+        if self.latest_model is None:
+            self.log.critical(f'Model.check() {self.model_name} not found in AWS Metadata!')
+            return False
+        return True
+
+    def uuid(self) -> str:
+        """The SageWorks Unique Identifier"""
+        return self.model_name
+
+    def size(self) -> bool:
+        """Return the size of this data in MegaBytes"""
+        return 0
+
+    def meta(self):
+        """Get the metadata for this artifact"""
+        return self.latest_model
+
+    def add_tag(self):
+        """Get the tags for this artifact"""
+        return []
+
+    def tags(self):
+        """Get the tags for this artifact"""
+        return self.model_tags
+
+    def aws_url(self):
+        """The AWS URL for looking at/querying this data source"""
+        return 'https://us-west-2.console.aws.amazon.com/athena/home'
+
+    def created(self) -> datetime:
+        """Return the datetime when this artifact was created"""
+        return self.latest_model['CreationTime']
+
+    def modified(self) -> datetime:
+        """Return the datetime when this artifact was last modified"""
+        return self.latest_model['CreationTime']
+
+    def delete(self):
+        """Delete the Model: Feature Group, Catalog Table, and S3 Storage Objects"""
+
+        # Delete the Model and ensure that it gets deleted
+        """
+        remove_model = FeatureGroup(name=self.model_name, sagemaker_session=self.sm_session)
+        remove_model.delete()
+        self.ensure_model_deleted(remove_model)
+        """
+
+    def ensure_model_deleted(self, feature_group):
+        """
+        status = feature_group.describe().get("FeatureGroupStatus")
+        while status == "Deleting":
+            self.log.info("Model being Deleted...")
+            time.sleep(5)
+            try:
+                status = feature_group.describe().get("FeatureGroupStatus")
+            except Exception:  # FIXME
+                break
+        self.log.info(f"Model {feature_group.name} successfully deleted")
+        """
+        pass
+
+
+# Simple test of the Model functionality
+def test():
+    """Test for Model Class"""
+
+    # Grab a Model object and pull some information from it
+    my_model = Model('solubility-regression')
+
+    # Call the various methods
+
+    # Lets do a check/validation of the Model
+    assert(my_model.check())
+
+    # Get the tags associated with this Model
+    print(f"Tags: {my_model.tags()}")
+
+
+if __name__ == "__main__":
+    test()
---
+++ src/sageworks/transforms/features_to_model/features_to_model.py
@@ -0,0 +1,121 @@
+"""FeaturesToModel: Train/Create a Model from a Feature Set"""
+import os
+import json
+from pathlib import Path
+from sagemaker.sklearn.estimator import SKLearn
+
+# Local Imports
+from sageworks.transforms.transform import Transform, TransformInput, TransformOutput
+from sageworks.artifacts.feature_sets.feature_set import FeatureSet
+
+
+class FeaturesToModel(Transform):
+    def __init__(self, input_uuid: str = None, output_uuid: str = None):
+        """FeaturesToModel: Train/Create a Model from a Feature Set"""
+
+        # Call superclass init
+        super().__init__(input_uuid, output_uuid)
+
+        # Set up all my instance attributes
+        self.input_type = TransformInput.FEATURE_SET
+        self.output_type = TransformOutput.MODEL
+        self.estimator = None
+        self.model_script_dir = None
+
+    def generate_model_script(self, target: str, feature_list: list[str], model_type) -> str:
+        """Fill in the model template with specific target and feature_list
+           Args:
+               target (str): Column name of the target variable
+               feature_list (list[str]): A list of columns for the features
+               model_type (str): regression or classification
+           Returns:
+              str: The name of the generated model script
+        """
+
+        # FIXME: Revisit all of this since it's a bit wonky
+        script_name = 'xgb_model.py'
+        dir_path = Path(__file__).parent.absolute()
+        self.model_script_dir = os.path.join(dir_path, 'light_model_harness')
+        template_path = os.path.join(self.model_script_dir, 'xgb_model.template')
+        output_path = os.path.join(self.model_script_dir, script_name)
+        with open(template_path, 'r') as fp:
+            xgb_template = fp.read()
+
+        # Template replacements
+        xgb_script = xgb_template.replace('{{target}}', target)
+        feature_list_str = json.dumps(feature_list)
+        xgb_script = xgb_script.replace('{{feature_list}}', feature_list_str)
+        xgb_script = xgb_script.replace('{{model_type}}', model_type)
+
+        # Now write out the generated model script and return the name
+        with open(output_path, 'w') as fp:
+            fp.write(xgb_script)
+        return script_name
+
+    def transform_impl(self, **kwargs):
+        """Compute a Feature Set based on RDKit Descriptors"""
+
+        # Get our Feature Set and create an S3 CSV Training dataset
+        feature_set = FeatureSet(self.input_uuid)
+        s3_training_path = feature_set.create_s3_training_data()
+
+        # Figure out features (FIXME)
+        all_columns = feature_set.column_names()
+        filter_list = ['id', 'smiles', 'eventtime', 'solubility', 'write_time', 'api_invocation_time', 'is_deleted']
+        feature_columns = [c for c in all_columns if c not in filter_list]
+
+        # Generate our model script
+        script_path = self.generate_model_script('solubility', feature_columns, 'regression')
+
+        # Create a Sagemaker Model with our script
+        self.estimator = SKLearn(
+            entry_point=script_path,
+            source_dir=self.model_script_dir,
+            role=self.sageworks_role_arn,
+            instance_type='ml.m5.large',
+            sagemaker_session=self.sm_session,
+            framework_version='1.0-1'
+        )
+
+        # Train the estimator
+        self.estimator.fit({'train': s3_training_path})
+
+        # Create Model and officially Register
+        self.create_and_register_model()
+
+    def create_and_register_model(self):
+        """Create and Register the Model"""
+
+        # Set up our information and tags
+        specs = {
+            "input": self.input_uuid,
+            "output": self.output_uuid,
+            "info": "Test Model: Solubility Regression",
+            "tags": ['sageworks']
+        }
+        model_specs = json.dumps(specs)
+        model = self.estimator.create_model(role=self.sageworks_role_arn)
+        model.register(
+            model_package_group_name='solubility-regression',
+            framework_version='1.0.1',
+            content_types=["text/csv"],
+            response_types=["text/csv"],
+            inference_instances=["ml.t2.medium"],
+            transform_instances=["ml.m5.large"],
+            approval_status="Approved",
+            description=model_specs
+        )
+
+
+# Simple test of the FeaturesToModel functionality
+def test():
+    """Test the FeaturesToModel Class"""
+
+    # Create the class with inputs and outputs and invoke the transform
+    input_uuid = 'test_rdkit_features'
+    output_uuid = 'test_solubility_regression'
+    FeaturesToModel(input_uuid, output_uuid).transform(delete_existing=True)
+
+
+if __name__ == "__main__":
+    test()
