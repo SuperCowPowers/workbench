c27f52ac0f01bf10b3849d6827e2d0a679179822
Brian Wylie <briford.wylie@gmail.com>
2023-03-30 21:36:45 -0600

    changing test to test_data/feature_set

--- src/sageworks/transforms/data_to_features/light/data_to_features_light.py
+++ src/sageworks/transforms/data_to_features/light/data_to_features_light.py
@@ -45,12 +45,12 @@ def test():
     """Test the DataToFeaturesLight Class"""
 
     # Create the class with inputs and outputs and invoke the transform
-    input_uuid = 'abalone_data'
-    output_uuid = 'abalone_feature_set'
+    input_uuid = 'test_data'
+    output_uuid = 'test_feature_set'
     data_to_features = DataToFeaturesLight(input_uuid, output_uuid)
-    data_to_features.set_output_tags(['abalone', 'public'])
+    data_to_features.set_output_tags(['test', 'small'])
     data_to_features.set_output_meta({'sageworks_input': input_uuid})
-    data_to_features.transform(delete_existing=True)
+    data_to_features.transform(id_column='id', event_time_column='date', delete_existing=True)
 
 
 if __name__ == "__main__":
