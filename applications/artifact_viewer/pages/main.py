e464763cdd5a434898c7df448ea37e52519b53e2
Brian Wylie <briford.wylie@gmail.com>
2023-04-07 10:09:32 -0600

    changing some variable names

--- applications/artifact_viewer/pages/main.py
+++ applications/artifact_viewer/pages/main.py
@@ -16,35 +16,35 @@ register_page(__name__, path="/")
 # Okay feels a bit weird but Dash pages just have a bunch of top level code (no classes/methods)
 
 # Grab a view that gives us a summary of all the artifacts currently in SageWorks
-sageworks_artifacts = ArtifactsSummary()
-artifacts_summary = sageworks_artifacts.view_data()
+artifacts_summary = ArtifactsSummary()
+sageworks_artifacts = artifacts_summary.view_data()
 
 # Grab the Artifact Information DataFrame for each AWS Service and pass it to the table creation
 tables = dict()
 tables["INCOMING_DATA"] = table.create(
     "INCOMING_DATA",
-    artifacts_summary["INCOMING_DATA"],
+    sageworks_artifacts["INCOMING_DATA"],
     header_color="rgb(60, 60, 100)",
 )
 tables["DATA_SOURCES"] = table.create(
     "DATA_SOURCES",
-    artifacts_summary["DATA_SOURCES"],
+    sageworks_artifacts["DATA_SOURCES"],
     header_color="rgb(100, 60, 60)",
     markdown_columns=["Name"],
 )
 tables["FEATURE_SETS"] = table.create(
     "FEATURE_SETS",
-    artifacts_summary["FEATURE_SETS"],
+    sageworks_artifacts["FEATURE_SETS"],
     header_color="rgb(100, 100, 60)",
     markdown_columns=["Feature Group"],
 )
 tables["MODELS"] = table.create(
     "MODELS",
-    artifacts_summary["MODELS"],
+    sageworks_artifacts["MODELS"],
     header_color="rgb(60, 100, 60)",
     markdown_columns=["Model Group"],
 )
-tables["ENDPOINTS"] = table.create("ENDPOINTS", artifacts_summary["ENDPOINTS"], header_color="rgb(100, 60, 100)")
+tables["ENDPOINTS"] = table.create("ENDPOINTS", sageworks_artifacts["ENDPOINTS"], header_color="rgb(100, 60, 100)")
 
 # Create our components
 components = {
@@ -57,7 +57,7 @@ components = {
 
 # Setup our callbacks/connections
 app = dash.get_app()
-callbacks.update_last_updated(app, sageworks_artifacts)
+callbacks.update_last_updated(app, artifacts_summary)
 callbacks.update_artifact_tables(app)
 
 # Set up our layout (Dash looks for a var called layout)
