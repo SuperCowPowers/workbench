bee03742c4e9df14bb1877e441897a0b50e2ffcc
Brian Wylie <briford.wylie@gmail.com>
2023-04-12 15:38:21 -0600

    fix aws account clamp test

--- tests/aws_account/aws_account_clamp_tests.py
+++ tests/aws_account/aws_account_clamp_tests.py
@@ -1,14 +1,14 @@
 """Tests for the AWS Account Clamp"""
 
 # SageWorks Imports
-from sageworks.aws_service_broker.aws_account_clamp import AWSAccountClamp
+from sageworks.aws_service_broker.aws_account_check import AWSAccountCheck
 
 
 def test():
     """Tests for the AWS Account Clamp"""
 
     # Get the AWS Account Clamp and make sure it checks out
-    aws_clamp = AWSAccountClamp()
+    aws_clamp = AWSAccountCheck()
     aws_clamp.check()
 
 
