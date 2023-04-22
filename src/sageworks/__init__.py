f1f67c876568ca498b0e0b5d21cca5072a1605e1
Brian Wylie <briford.wylie@gmail.com>
2023-04-22 17:28:00 -0600

    adding some top level help instructions

--- src/sageworks/__init__.py
+++ src/sageworks/__init__.py
@@ -1,4 +1,31 @@
 # Copyright (c) 2021-2023 SuperCowPowers LLC
 
-# Licensed under the MIT License
-# See: https://github.com/SuperCowPowers/sageworks/blob/main/LICENSE
+"""
+SageWorks Main Classes
+- Artifacts
+  - DataSource
+  - FeatureSet
+  - Model
+  - Endpoint
+- Transforms
+  - DataLoaders
+  - DataToData
+  - DataToFeatures
+  - FeaturesToModel
+  - ModelToEndpoint
+
+  For help on particular classes you can do this
+  - from sageworks.transforms.data_loaders.light.json_to_data_source import JSONToDataSource
+  - help(JSONToDataSource)
+
+
+      class JSONToDataSource(sageworks.transforms.transform.Transform)
+     |  JSONToDataSource(json_file_path: str, data_uuid: str)
+     |
+     |  JSONToDataSource: Class to move local JSON Files into a SageWorks DataSource
+     |
+     |  Common Usage:
+     |      json_to_data = JSONToDataSource(json_file_path, data_uuid)
+     |      json_to_data.set_output_tags(["abalone", "json", "whatever"])
+     |      json_to_data.transform()
+"""
