c18dcf636f707d790ce4010addd227a00b8b37af
Brian Wylie <briford.wylie@gmail.com>
2023-04-23 17:36:21 -0600

    initial version of the Glue Heavy Data Loader

--- src/sageworks/transforms/data_loaders/heavy/s3_heavy_to_data_source.py
+++ src/sageworks/transforms/data_loaders/heavy/s3_heavy_to_data_source.py
@@ -1,18 +1,22 @@
 """S3HeavyToDataSource: Class to move HEAVY S3 Files into a SageWorks DataSource"""
-import awswrangler as wr
+import sys
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.context import SparkContext
 
-# Local imports
+# SageWorks imports
 from sageworks.transforms.transform import Transform, TransformInput, TransformOutput
-from sageworks.artifacts.data_sources.athena_source import AthenaSource
-
-
-# FIXME: This class is not funny implemented. It will be a glue jobs that uses SPARK to convert
-#        CSV to Parquet in a scalable way and then populate the AWS Data Catalog with the information
 
 
 class S3HeavyToDataSource(Transform):
-    def __init__(self, input_uuid, output_uuid):
-        """S3HeavyToDataSource: Class to move HEAVY S3 Files into a SageWorks DataSource"""
+    def __init__(self, job_name: str, input_uuid: str, output_uuid: str):
+        """S3HeavyToDataSource: Class to move HEAVY S3 Files into a SageWorks DataSource
+        Args:
+            job_name (str): The name of the AWS Glue Job
+            input_uuid (str): The S3 Path to the files to be loaded
+            output_uuid (str): The UUID of the SageWorks DataSource to be created
+        """
 
         # Call superclass init
         super().__init__(input_uuid, output_uuid)
@@ -21,67 +25,75 @@ class S3HeavyToDataSource(Transform):
         self.input_type = TransformInput.S3_OBJECT
         self.output_type = TransformOutput.DATA_SOURCE
 
+        # These are AWS Glue Job specific
+        sc = SparkContext()
+        self.glueContext = GlueContext(sc)
+        self.job = Job(self.glueContext)
+        self.job.init(job_name, [job_name, input_uuid, output_uuid])
+
     def transform_impl(self, overwrite: bool = True):
         """Convert the CSV data into Parquet Format in the SageWorks S3 Bucket, and
         store the information about the data to the AWS Data Catalog sageworks database"""
 
         # Add some tags here
-        tags = ["sageworks", "public"]
-
-        # Read in the S3 CSV as a Pandas DataFrame
-        df = wr.s3.read_csv(self.input_uuid, low_memory=False, boto3_session=self.boto_session)
-
-        # Set up our metadata storage
-        sageworks_meta = {"sageworks_tags": tags}
-        for key, value in self.output_meta.items():
-            sageworks_meta[key] = value
+        tags = ["heavy"]
 
         # Create the Output Parquet file S3 Storage Path
         s3_storage_path = f"{self.data_source_s3_path}/{self.output_uuid}"
 
-        # Write out the DataFrame to Parquet/DataStore/Athena
+        # Read JSONL files from S3 and infer schema dynamically
+        self.log.info(f"Reading JSONL files from {self.input_uuid}...")
+        jsonl_dyf = self.glueContext.create_dynamic_frame_from_options(
+            connection_type="s3",
+            connection_options={"paths": [self.input_uuid],
+                                "compressionType": "gzip",
+                                "recurse": "True",
+                                },
+            format="json",
+            format_options={'jsonPath': 'auto'},
+            transformation_ctx='apply_mapping'
+        )
+
+        # Write Parquet files to S3
+        self.log.info(f"Writing Parquet files to {s3_storage_path}...")
+        self.glueContext.write_dynamic_frame.from_options(
+            frame=jsonl_dyf,
+            connection_type='s3',
+            connection_options={'path': s3_storage_path},
+            format='parquet',
+            format_options={'compression': 'snappy', 'parquetVersion': '2.0'},
+            transformation_ctx='datasink'
+        )
+
+        # Set up our SageWorks metadata
+        sageworks_meta = {"sageworks_tags": tags}
+        for key, value in self.output_meta.items():
+            sageworks_meta[key] = value
+
+        # Generate a Glue Catalog Table
         description = f"SageWorks data source: {self.output_uuid}"
-        glue_table_settings = {"description": description, "parameters": sageworks_meta}
-        wr.s3.to_parquet(
-            df,
-            path=s3_storage_path,
-            dataset=True,
-            mode="overwrite",
-            database=self.data_catalog_db,
-            table=self.output_uuid,
-            filename_prefix=f"{self.output_uuid}_",
-            boto3_session=self.boto_session,
-            partition_cols=None,
-            glue_table_settings=glue_table_settings,
-        )  # FIXME: Have some logic around partition columns
+        self.glueContext.catalog.create_table(
+            database='sageworks',  # FIXME: Have this in config
+            table_name=self.output_uuid,
+            location=s3_storage_path,
+            schema=jsonl_dyf.schema(),
+            description=description,
+            parameters=sageworks_meta
+        )
+
+        # Commit the Glue Job
+        self.log.info(f"{self.input_uuid} --> {self.output_uuid} complete!")
+        self.job.commit()
 
 
 if __name__ == "__main__":
-    """Exercise the S3HeavyToDataSource Class"""
-    import pandas as pd
-    from sageworks.utils.sageworks_config import SageWorksConfig
+    """Glue Job for the S3HeavyToDataSource Class"""
 
-    # Create my Data Loader
-    sageworks_config = SageWorksConfig()
-    sageworks_bucket = sageworks_config.get_config_value("SAGEWORKS_AWS", "S3_BUCKET")
-    incoming_data_bucket = sageworks_bucket + "/incoming-data"
-    incoming_data = incoming_data_bucket + "/abalone.csv"
+    # Get the arguments for this Glue Job
+    args = getResolvedOptions(sys.argv, ["JOB_NAME", "SRC_PATH", "OUTPUT_UUID"])
 
-    # Create my Data Loader
-    output_uuid = "test_heavy_data"
-    my_loader = S3HeavyToDataSource(incoming_data, output_uuid)
+    # Create the Data Loader
+    my_loader = S3HeavyToDataSource(args['JOB_NAME'], args['SRC_PATH'], args['OUTPUT_UUID'])
 
     # Store this data as a SageWorks DataSource
     my_loader.transform()
-
-    # Grab the output and query it for a dataframe
-    output = AthenaSource(output_uuid)
-    df = output.query(f"select * from {output_uuid} limit 5")
-
-    # Setup Pandas output options
-    pd.set_option("display.max_colwidth", 15)
-    pd.set_option("display.max_columns", 15)
-    pd.set_option("display.width", 1000)
-
-    # Show the dataframe
-    print(df)
