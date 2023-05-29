9623b31f5a5896b6d59c9d7a439ac81e79644e03
Brian Wylie <briford.wylie@gmail.com>
2023-05-29 13:17:30 -0600

    a set of empty data sanity checks for when there are ZERO artifacts

--- applications/aws_dashboard/pages/callbacks/main_callbacks.py
+++ applications/aws_dashboard/pages/callbacks/main_callbacks.py
@@ -23,25 +23,35 @@ def update_last_updated(app: Dash, sageworks_artifacts: ArtifactsWebView):
 def update_artifact_tables(app: Dash):
     @app.callback(Output("INCOMING_DATA", "data"), Input("main-updater", "n_intervals"))
     def incoming_data_update(n):
+        if all_data is None:
+            return {}
         incoming_data = all_data["INCOMING_DATA"]
         return incoming_data.to_dict("records")
 
     @app.callback(Output("DATA_SOURCES", "data"), Input("main-updater", "n_intervals"))
     def data_sources_update(n):
+        if all_data is None:
+            return {}
         data_sources = all_data["DATA_SOURCES"]
         return data_sources.to_dict("records")
 
     @app.callback(Output("FEATURE_SETS", "data"), Input("main-updater", "n_intervals"))
     def feature_sets_update(n):
+        if all_data is None:
+            return {}
         feature_sets = all_data["FEATURE_SETS"]
         return feature_sets.to_dict("records")
 
     @app.callback(Output("MODELS", "data"), Input("main-updater", "n_intervals"))
     def models_update(n):
+        if all_data is None:
+            return {}
         models = all_data["MODELS"]
         return models.to_dict("records")
 
     @app.callback(Output("ENDPOINTS", "data"), Input("main-updater", "n_intervals"))
     def endpoints_update(n):
+        if all_data is None:
+            return {}
         endpoints = all_data["ENDPOINTS"]
         return endpoints.to_dict("records")
