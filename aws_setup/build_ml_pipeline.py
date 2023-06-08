edd34a8954ef8ee37ce8769b72470d79d1a281c1
giupb <giulio.pbrugalli@gmail.com>
2023-06-08 09:29:00 -0300

    uncomment pipeline

--- aws_setup/build_ml_pipeline.py
+++ aws_setup/build_ml_pipeline.py
@@ -57,45 +57,45 @@ if __name__ == "__main__":
         print("Waiting for the test_data to be created...")
         time.sleep(10)
 
-    # # Create the abalone_data DataSource
-    # if not DataSource("abalone_data").check():
-    #     my_loader = CSVToDataSource(abalone_data_path, "abalone_data")
-    #     my_loader.set_output_tags("abalone:public")
-    #     my_loader.transform()
-    #     print("Waiting for the abalone_data to be created...")
-    #     time.sleep(10)
+    # Create the abalone_data DataSource
+    if not DataSource("abalone_data").check():
+        my_loader = CSVToDataSource(abalone_data_path, "abalone_data")
+        my_loader.set_output_tags("abalone:public")
+        my_loader.transform()
+        print("Waiting for the abalone_data to be created...")
+        time.sleep(10)
 
-    # # Give user a warning about how long the rest of the script will take
-    # print("\n******************************************************************************")
-    # print("Note: The rest of this script takes about 20 minutes (AWS finalize/wait times)")
-    # print("******************************************************************************\n")
-    # time.sleep(5)
+    # Give user a warning about how long the rest of the script will take
+    print("\n******************************************************************************")
+    print("Note: The rest of this script takes about 20 minutes (AWS finalize/wait times)")
+    print("******************************************************************************\n")
+    time.sleep(5)
 
-    # # Create the test_feature_set FeatureSet
-    # if not FeatureSet("test_feature_set").check():
-    #     data_to_features = DataToFeaturesLight("test_data", "test_feature_set")
-    #     data_to_features.set_output_tags(["test", "small"])
-    #     data_to_features.transform(id_column="id", event_time_column="date")
+    # Create the test_feature_set FeatureSet
+    if not FeatureSet("test_feature_set").check():
+        data_to_features = DataToFeaturesLight("test_data", "test_feature_set")
+        data_to_features.set_output_tags(["test", "small"])
+        data_to_features.transform(id_column="id", event_time_column="date")
 
-    # # Create the abalone_feature_set FeatureSet
-    # if not FeatureSet("abalone_feature_set").check():
-    #     data_to_features = DataToFeaturesLight("abalone_data", "abalone_feature_set")
-    #     data_to_features.set_output_tags(["abalone", "public"])
-    #     data_to_features.transform()
+    # Create the abalone_feature_set FeatureSet
+    if not FeatureSet("abalone_feature_set").check():
+        data_to_features = DataToFeaturesLight("abalone_data", "abalone_feature_set")
+        data_to_features.set_output_tags(["abalone", "public"])
+        data_to_features.transform()
 
-    # # Create the abalone_regression Model
-    # if not Model("abalone-regression").check():
-    #     features_to_model = FeaturesToModel("abalone_feature_set", "abalone-regression")
-    #     features_to_model.set_output_tags(["abalone", "regression"])
-    #     features_to_model.transform(target="class_number_of_rings", description="Abalone Regression Model")
+    # Create the abalone_regression Model
+    if not Model("abalone-regression").check():
+        features_to_model = FeaturesToModel("abalone_feature_set", "abalone-regression")
+        features_to_model.set_output_tags(["abalone", "regression"])
+        features_to_model.transform(target="class_number_of_rings", description="Abalone Regression Model")
 
-    # # Wait for the Model to be created
-    # while not Model("abalone-regression").check():
-    #     print("Waiting for the Model to be created...")
-    #     time.sleep(5)
+    # Wait for the Model to be created
+    while not Model("abalone-regression").check():
+        print("Waiting for the Model to be created...")
+        time.sleep(5)
 
-    # # Create the abalone_regression Endpoint
-    # if not Endpoint("abalone-regression-end").check():
-    #     model_to_endpoint = ModelToEndpoint("abalone-regression", "abalone-regression-end")
-    #     model_to_endpoint.set_output_tags(["abalone", "regression"])
-    #     model_to_endpoint.transform()
+    # Create the abalone_regression Endpoint
+    if not Endpoint("abalone-regression-end").check():
+        model_to_endpoint = ModelToEndpoint("abalone-regression", "abalone-regression-end")
+        model_to_endpoint.set_output_tags(["abalone", "regression"])
+        model_to_endpoint.transform()
