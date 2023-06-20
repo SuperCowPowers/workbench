ec4ffe00780bd891a014b8bafb8832bc1be46a09
Brian Wylie <briford.wylie@gmail.com>
2023-06-20 11:16:12 -0600

    removing the old web_components classes and adding some 'fake_it' logic to the scatter plot if the dataframe doesn't have clusters/x/y

--- src/sageworks/web_components/scatter_plot.py
+++ src/sageworks/web_components/scatter_plot.py
@@ -2,12 +2,20 @@
 from dash import dcc
 import pandas as pd
 import plotly.express as px
+import numpy as np
 
 
 # For colormaps see (https://plotly.com/python/discrete-color/#color-sequences-in-plotly-express)
 def create(plot_id: str, df: pd.DataFrame) -> dcc.Graph:
     """Create a Scatter Plot"""
 
+    # Fake data if it's not in the dataframe
+    if "cluster" not in df.columns:
+        df["cluster"] = np.random.randint(0, 10, df.shape[0])
+        df["x"] = np.random.rand(df.shape[0])
+        df["y"] = np.random.rand(df.shape[0])
+
+    # Create the Scatter Plot
     color_map = px.colors.qualitative.Plotly
     fig = px.scatter(
         df,
