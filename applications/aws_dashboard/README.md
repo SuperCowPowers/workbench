18fee59e74ebf383056c20b1a17e933191430e3b
Brian Wylie <briford.wylie@gmail.com>
2023-09-12 16:22:17 -0600

    setting up a nginx/wsgi docker image for running the SageWorks Dashboard application

--- applications/aws_dashboard/README.md
+++ applications/aws_dashboard/README.md
@@ -8,10 +8,23 @@ $ pip install -r requirements.txt
 $ python app.py
 ``` 
 
-Or you can run it in a Docker container:
+## Docker and Getting SSO Token
+In general you should talk to one of the SageWorks developers if you want to do local testing with docker. This is a development corner case, and 99.9% of folks won't need this.
 
-```bash
-$ cd applications/aws_dashboard
-$ docker build -t aws_dashboard .
-$ docker run -p 8050:8050 aws_dashboard
+But just for reference:
+
+```
+aws sts assume-role --role-arn  
+arn:aws:iam::<account_nam>:role/SageWorks-ExecutionRole 
+--role-session-name "DockerSession"
+```
+Take the output from this and fill in the command below
+
+```
+docker run -p 8000:8000 \
+  -e AWS_ACCESS_KEY_ID=<AccessKeyId> \
+  -e AWS_SECRET_ACCESS_KEY=<SecretAccessKey> \
+  -e AWS_SESSION_TOKEN=<SessionToken> \
+  -e AWS_DEFAULT_REGION=us-west-2 \
+  sageworks_dashboard
 ```
--- applications/aws_dashboard/app.py
+++ applications/aws_dashboard/app.py
@@ -1,4 +1,5 @@
 """Artifact Viewer: A SageWorks Application for viewing and managing SageWorks Artifacts"""
+import os
 from dash import Dash
 import dash
 import dash_bootstrap_components as dbc
@@ -9,19 +10,21 @@ import dash_bootstrap_components as dbc
 # Note: The 'app' and 'server' objects need to be at the top level since NGINX/uWSGI needs to
 #       import this file and use the server object as an ^entry-point^ into the Dash Application Code
 
+# When run on docker we need to find the absolute path the pages folder
+current_dir = os.path.dirname(os.path.abspath(__file__))
+pages_folder = os.path.join(current_dir, "pages")
+
 # Create our Dash Application
-# app = Dash(title='SageWorks: Artifacts', external_stylesheets=[dbc.themes.BOOTSTRAP], use_pages=True)
-# app = Dash(title="SageWorks: Artifacts", use_pages=True)
 app = Dash(
     title="SageWorks Dashboard",
     external_stylesheets=[dbc.themes.DARKLY],
     use_pages=True,
+    pages_folder=pages_folder
 )
 server = app.server
 
 # For Multi-Page Applications, we need to create a 'page container' to hold all the pages
 app.layout = dash.page_container
-
 
 if __name__ == "__main__":
     """Run our web application in TEST mode"""
