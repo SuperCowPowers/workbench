399b74b06a035630a8e286c1c74ac34b81fbae06
Brian Wylie <briford.wylie@gmail.com>
2023-09-12 17:37:16 -0600

    adding the __name__ parameter when creating the Dash app, it sets up the application structure and main/relative directories, so that assets/ pages/ etc get located

--- applications/aws_dashboard/app.py
+++ applications/aws_dashboard/app.py
@@ -10,16 +10,12 @@ import dash_bootstrap_components as dbc
 # Note: The 'app' and 'server' objects need to be at the top level since NGINX/uWSGI needs to
 #       import this file and use the server object as an ^entry-point^ into the Dash Application Code
 
-# When run on docker we need to find the absolute path the pages folder
-current_dir = os.path.dirname(os.path.abspath(__file__))
-pages_folder = os.path.join(current_dir, "pages")
-
 # Create our Dash Application
 app = Dash(
+    __name__,
     title="SageWorks Dashboard",
     external_stylesheets=[dbc.themes.DARKLY],
     use_pages=True,
-    pages_folder=pages_folder
 )
 server = app.server
 
