71c907ea3c9c5140c6c2067ec84e01d345942ae2
Brian Wylie <briford.wylie@gmail.com>
2023-09-13 15:10:04 -0600

    updating the CDK script to include logging and a Load Balancer

--- aws_setup/sageworks_dashboard/sageworks_dashboard/sageworks_dashboard_stack.py
+++ aws_setup/sageworks_dashboard/sageworks_dashboard/sageworks_dashboard_stack.py
@@ -1,6 +1,15 @@
 import os
-from aws_cdk import Stack, aws_ecs as ecs, aws_ec2 as ec2, aws_iam as iam, aws_elasticache as elasticache
+from aws_cdk import (
+    Stack,
+    aws_ecs as ecs,
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    aws_elasticache as elasticache,
+    aws_logs as logs,
+)
+from aws_cdk.aws_ecs_patterns import ApplicationLoadBalancedFargateService
 from constructs import Construct
+
 
 sageworks_bucket = os.environ.get("SAGEWORKS_BUCKET")
 
@@ -14,15 +23,20 @@ class SageworksDashboardStack(Stack):
 
         # Import the existing SageWorks-ExecutionRole
         sageworks_execution_role = iam.Role.from_role_arn(
-            self, "ImportedSageWorksExecutionRole", f"arn:aws:iam::{self.account}:role/SageWorks-ExecutionRole"
+            self, "ImportedSageWorksExecutionRole",
+            f"arn:aws:iam::{self.account}:role/SageWorks-ExecutionRole"
         )
+
+        # Setup CloudWatch logs
+        log_group = logs.LogGroup(self, "SageWorksLogGroup")
 
         # Setup Redis
         redis_security_group = ec2.SecurityGroup(self, "RedisSecurityGroup", vpc=cluster.vpc)
 
         # Allow the ECS task to connect to the Redis cluster
         redis_security_group.add_ingress_rule(
-            peer=ec2.Peer.ipv4(cluster.vpc.vpc_cidr_block), connection=ec2.Port.tcp(6379)
+            peer=ec2.Peer.ipv4(cluster.vpc.vpc_cidr_block),
+            connection=ec2.Port.tcp(6379)
         )
 
         # Create the Redis subnet group
@@ -61,14 +75,23 @@ class SageworksDashboardStack(Stack):
             "SageworksContainer",
             image=ecs.ContainerImage.from_registry("public.ecr.aws/m6i5k1r2/sageworks_dashboard:latest"),
             memory_limit_mib=4096,
-            environment={"REDIS_HOST": redis_endpoint, "SAGEWORKS_BUCKET": sageworks_bucket},
+            environment={
+                "REDIS_HOST": redis_endpoint,
+                "SAGEWORKS_BUCKET": sageworks_bucket},
+            logging=ecs.LogDriver.aws_logs(
+                stream_prefix="SageWorksDashboard",
+                log_group=log_group)
         )
         container.add_port_mappings(ecs.PortMapping(container_port=8000))
 
-        # Define the ECS service
-        ecs.FargateService(
+        # Adding LoadBalancer with Fargate Service
+        ApplicationLoadBalancedFargateService(
             self,
             "SageworksService",
             cluster=cluster,
+            cpu=2048,
+            desired_count=1,
             task_definition=task_definition,
+            memory_limit_mib=4096,
+            public_load_balancer=False
         )
