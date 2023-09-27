00f9e991a42afcb1ace5f46370efa95f38084adf
Brian Wylie <briford.wylie@gmail.com>
2023-09-27 10:36:41 -0600

    having the sageworks logging setup be a 'singleton' and a bit better about not affecting global logging

--- src/sageworks/utils/sageworks_logging.py
+++ src/sageworks/utils/sageworks_logging.py
@@ -1,17 +1,19 @@
 """Setup logging defaults"""
 import logging
 import os
+from functools import lru_cache
 
 
+@lru_cache(maxsize=None) # Cache the logging setup
 def logging_setup():
-    """This will setup the default formatters and handlers for logging"""
-    logging.basicConfig(
-        stream=os.sys.stdout,
-        level=os.environ.get("LOGLEVEL", "INFO"),
-        format="%(asctime)s (%(filename)s:%(lineno)d) %(levelname)s %(message)s",
+    logger = logging.getLogger("sageworks")
+    logger.setLevel(os.environ.get("LOGLEVEL", "INFO"))
+
+    handler = logging.StreamHandler(os.sys.stdout)
+    formatter = logging.Formatter(
+        "%(asctime)s (%(filename)s:%(lineno)d) %(levelname)s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
-    # Make boto be more quiet
-    logging.getLogger("boto3").setLevel(logging.WARNING)
-    logging.getLogger("botocore").setLevel(logging.WARNING)
