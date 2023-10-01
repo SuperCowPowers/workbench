2a945ee9ff4a0eb5d0dfd928b5d5bebfc3c37d8e
Brian Wylie <briford.wylie@gmail.com>
2023-10-01 11:36:02 -0600

    script for creating classification artifacts

--- src/sageworks/transforms/data_to_features/light/data_to_features_light.py
+++ src/sageworks/transforms/data_to_features/light/data_to_features_light.py
@@ -26,6 +26,7 @@ class DataToFeaturesLight(Transform):
         self.output_type = TransformOutput.FEATURE_SET
         self.input_df = None
         self.output_df = None
+        self.target = None
 
     def pre_transform(self, **kwargs):
         """Pull the input DataSource into our Input Pandas DataFrame"""
@@ -33,15 +34,17 @@ class DataToFeaturesLight(Transform):
         # Grab the Input (Data Source)
         self.input_df = DataToPandas(self.input_uuid).get_output()  # Shorthand for transform, get_output
 
-    def transform_impl(self, query: str = None, column_select: list = None, **kwargs):
+    def transform_impl(self, target: str, query: str = None, column_select: list = None, **kwargs):
         """Optional Query to filter the input DataFrame, then publish to the output location
         Args:
+            target(str): The name of the target column
             query(str): Optional query to filter the input DataFrame
             column_select(list): Optional list of columns to select from the input DataFrame
         Notes:
             Query is a Pandas Expression, e.g. 'col1<2.5 & col2=="x"'
             Column Select is a list of column names, e.g. ['col3', 'col4']
         """
+        self.target = target
         self.output_df = self.input_df.query(query).reset_index(drop=True) if query else self.input_df
         self.output_df = self.output_df[column_select] if column_select else self.output_df
 
@@ -49,7 +52,7 @@ class DataToFeaturesLight(Transform):
         """At this point the output DataFrame should be populated, so publish it as a Feature Set"""
         # Now publish to the output location
         output_features = PandasToFeatures(self.output_uuid)
-        output_features.set_input(self.output_df, id_column=id_column, event_time_column=event_time_column)
+        output_features.set_input(self.output_df, self.target, id_column=id_column, event_time_column=event_time_column)
         output_features.set_output_tags(self.output_tags)
         output_features.add_output_meta(self.output_meta)
         output_features.transform()
--- tests/create_test_artifacts_classification.py
+++ tests/create_test_artifacts_classification.py
@@ -12,50 +12,46 @@ import time
 import pandas as pd
 
 from pathlib import Path
+from sageworks.artifacts.data_sources.data_source import DataSource
 from sageworks.artifacts.feature_sets.feature_set import FeatureSet
 from sageworks.artifacts.models.model import Model
 from sageworks.artifacts.endpoints.endpoint import Endpoint
 
-from sageworks.transforms.pandas_transforms.data_to_pandas import DataToPandas
-from sageworks.transforms.pandas_transforms.pandas_to_features import PandasToFeatures
+from sageworks.transforms.data_loaders.light.csv_to_data_source import CSVToDataSource
+from sageworks.transforms.data_to_features.light.data_to_features_light import DataToFeaturesLight
 from sageworks.transforms.features_to_model.features_to_model import FeaturesToModel
 from sageworks.transforms.model_to_endpoint.model_to_endpoint import ModelToEndpoint
 
 if __name__ == "__main__":
     # Get the path to the dataset in the repository data directory
-    abalone_data_path = Path(sys.modules["sageworks"].__file__).parent.parent.parent / "data" / "abalone.csv"
+    wine_data_path = Path(sys.modules["sageworks"].__file__).parent.parent.parent / "data" / "wine_classification.csv"
 
-    # Create the abalone_classification FeatureSet
-    if not FeatureSet("abalone_classification").exists():
-        # Grab the data from the DataSource
-        data_to_pandas = DataToPandas("abalone_data")
-        data_to_pandas.transform()
-        df = data_to_pandas.get_output()
+    # Create the abalone_data DataSource
+    if not DataSource("wine_data").exists():
+        my_loader = CSVToDataSource(wine_data_path, "wine_data")
+        my_loader.set_output_tags("wine:classification")
+        my_loader.transform()
+        print("Waiting for the wine_data to be created...")
+        time.sleep(5)
 
-        # Convert the regression target to a categorical type and drop the old column
-        bins = [0, 7, 12, float("inf")]
-        labels = ["young", "adult", "old"]
-        df["clam_age_class"] = pd.cut(df["class_number_of_rings"], bins=bins, labels=labels)
-        df.drop("class_number_of_rings", axis=1, inplace=True)
-
-        # Create the FeatureSet
-        pandas_to_features = PandasToFeatures("abalone_classification")
-        pandas_to_features.set_input(df, target_column="clam_age_class")
-        pandas_to_features.set_output_tags(["abalone", "classification"])
-        pandas_to_features.transform()
+    # Create the wine_feature_set FeatureSet
+    if not FeatureSet("wine_feature_set").exists():
+        data_to_features = DataToFeaturesLight("wine_data", "wine_feature_set")
+        data_to_features.set_output_tags(["wine", "classification"])
+        data_to_features.transform(target="target")
 
     # Create the abalone_classification Model
-    if not Model("abalone-classification").exists():
-        features_to_model = FeaturesToModel("abalone_classification", "abalone-classification")
-        features_to_model.set_output_tags(["abalone", "classification"])
+    if not Model("wine-classification").exists():
+        features_to_model = FeaturesToModel("wine_feature_set", "wine-classification")
+        features_to_model.set_output_tags(["wine", "classification"])
         features_to_model.transform(
-            target="clam_age_class", description="Abalone Classification Model", model_type="classifier"
+            target="target", description="Wine Classification Model", model_type="classifier"
         )
         print("Waiting for the Model to be created...")
         time.sleep(10)
 
     # Create the abalone_regression Endpoint
-    if not Endpoint("abalone-classification-end").exists():
-        model_to_endpoint = ModelToEndpoint("abalone-classification", "abalone-classification-end")
-        model_to_endpoint.set_output_tags(["abalone", "classification"])
+    if not Endpoint("wine-classification-end").exists():
+        model_to_endpoint = ModelToEndpoint("wine-classification", "wine-classification-end")
+        model_to_endpoint.set_output_tags(["wine", "classification"])
         model_to_endpoint.transform()
