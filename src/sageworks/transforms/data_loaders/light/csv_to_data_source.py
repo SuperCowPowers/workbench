6f354175e06cf5906aef8ae188bc5390c5a4703c
Brian Wylie <briford.wylie@gmail.com>
2023-10-02 09:12:18 -0600

    removing an unused import

--- src/sageworks/transforms/data_loaders/light/csv_to_data_source.py
+++ src/sageworks/transforms/data_loaders/light/csv_to_data_source.py
@@ -1,7 +1,6 @@
 """CSVToDataSource: Class to move local CSV Files into a SageWorks DataSource"""
 import os
 import pandas as pd
-from pandas.errors import ParserError
 
 # Local imports
 from sageworks.transforms.transform import Transform, TransformInput, TransformOutput
