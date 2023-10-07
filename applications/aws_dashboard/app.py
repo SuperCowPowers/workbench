ea21777622e555c828589d52634947c6a4354c2c
Brian Wylie <briford.wylie@gmail.com>
2023-10-07 13:05:42 -0600

    taking the app out of debug mode

--- applications/aws_dashboard/app.py
+++ applications/aws_dashboard/app.py
@@ -28,5 +28,5 @@ app.layout = html.Div(
 if __name__ == "__main__":
     """Run our web application in TEST mode"""
     # Note: This 'main' is purely for running/testing locally
-    app.run_server(host="0.0.0.0", port=8000, debug=True)
-    # app.run_server(host="0.0.0.0", port=8000)
+    # app.run_server(host="0.0.0.0", port=8000, debug=True)
+    app.run_server(host="0.0.0.0", port=8000)
