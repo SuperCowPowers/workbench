44230be4ffdfe5b426717337cfc2586c45ac2f0b
Brian Wylie <briford.wylie@gmail.com>
2023-10-23 12:17:34 -0600

    adding a trace level and colored output

--- src/sageworks/utils/sageworks_logging.py
+++ src/sageworks/utils/sageworks_logging.py
@@ -2,11 +2,51 @@ import os
 import logging
 
 
-def logging_setup():
+import os
+import logging
+
+
+# Define TRACE level
+# Note: see https://docs.python.org/3/library/logging.html#logging-levels
+TRACE_LEVEL_NUM = 25  # Between INFO and WARNING
+logging.addLevelName(TRACE_LEVEL_NUM, "TRACE")
+
+
+def trace(self, message, *args, **kws):
+    if self.isEnabledFor(TRACE_LEVEL_NUM):
+        self._log(TRACE_LEVEL_NUM, message, args, **kws)
+
+
+# Add the trace level to the logger
+logging.Logger.trace = trace
+
+
+# Define a ColoredFormatter
+class ColoredFormatter(logging.Formatter):
+    COLORS = {
+        'TRACE': '\033[38;5;141m',  # LightPurple
+        'DEBUG': '\033[38;5;111m',  # LightBlue
+        'INFO': '\033[38;5;113m',   # LightGreen
+        'WARNING': '\033[38;5;220m',  # Yellow
+        'ERROR': '\033[38;5;202m',  # Orange?
+        'CRITICAL': '\033[38;5;196m',  # Red
+    }
+
+    RESET = '\033[0m'
+
+    def format(self, record):
+        log_message = super().format(record)
+        return f"{self.COLORS.get(record.levelname, self.RESET)}{log_message}{self.RESET}"
+
+
+def logging_setup(color_logs=True):
     log = logging.getLogger("sageworks")
     if not log.hasHandlers():
         handler = logging.StreamHandler()
-        formatter = logging.Formatter(
+        formatter = ColoredFormatter(
+            "%(asctime)s (%(filename)s:%(lineno)d) %(levelname)s %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        ) if color_logs else logging.Formatter(
             "%(asctime)s (%(filename)s:%(lineno)d) %(levelname)s %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
         )
@@ -29,13 +69,24 @@ def logging_setup():
 
 
 if __name__ == "__main__":
+    import time
     # Uncomment to test the SAGEWORKS_DEBUG env variable
     # os.environ["SAGEWORKS_DEBUG"] = "True"
 
     logging_setup()
     my_log = logging.getLogger("sageworks")
-    my_log.info("Info: You should see me")
-    my_log.debug("Debug: You should see me only if SAGEWORKS_DEBUG is True")
+    my_log.info("You should see me")
+    my_log.debug("You should see me only if SAGEWORKS_DEBUG is True")
     logging.getLogger("sageworks").setLevel(logging.WARNING)
-    my_log.info("Info: You should NOT see me")
-    my_log.warning("Warning: You should see me")
+    my_log.info("You should NOT see me")
+    my_log.warning("You should see me")
+
+    # Test out ALL the colors
+    logging.getLogger("sageworks").setLevel(logging.DEBUG)
+    my_log.debug("\n\nColorized Logging...")
+    my_log.debug("This should be a muted color")
+    my_log.info("This should be a nice color")
+    my_log.trace("This color should stand out from debug and info")
+    my_log.warning("This should be a color that attracts attention")
+    my_log.error("This should be a bright, alert color")
+    my_log.critical("This should be a bright, alert color")
