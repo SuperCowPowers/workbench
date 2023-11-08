45fd4a083a232708afeaaf95a5a4128abd7da533
Brian Wylie <briford.wylie@gmail.com>
2023-11-07 21:02:02 -0700

    just some tweaks to the table header

--- src/sageworks/web_components/table.py
+++ src/sageworks/web_components/table.py
@@ -52,9 +52,12 @@ class Table(ComponentInterface):
                 "textAlign": "left",
             },
             style_header={
+                'className': 'dash-table-header',
                 "textAlign": "left",
                 "fontSize": 16,
+                'padding': '3px 5px 7px 5px',
                 "backgroundColor": header_color,
+                'backgroundImage': f"linear-gradient(to bottom, {header_color}, rgba(0, 0, 0, 0.6))",
                 "color": "rgb(200, 200, 200)",
             },
             style_data={
