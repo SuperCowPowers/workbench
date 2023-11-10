bb2501c3d1e1cc0ea8edd3fa63bd56f1da750d5d
Brian Wylie <briford.wylie@gmail.com>
2023-11-09 20:32:05 -0700

    scripts for bulk make_ready operations

---
+++ scripts/endpoints_ready.py
@@ -0,0 +1,22 @@
+"""Script that loops through all endpoints and checks if they are ready"""
+import logging
+
+# SageWorks Imports
+from sageworks.views.artifacts_text_view import ArtifactsTextView
+from sageworks.artifacts.endpoints.endpoint import Endpoint
+
+# Setup logging
+log = logging.getLogger("sageworks")
+
+# Create a instance of the ArtifactsTextView
+artifacts_text_view = ArtifactsTextView()
+
+# Get all the endpoints
+endpoints = artifacts_text_view.endpoints_summary()
+for end_name in endpoints["Name"]:
+    end = Endpoint(end_name)
+    if end.ready():
+        log.debug(f"Endpoint {end_name} is ready!")
+    else:
+        log.important(f"Endpoint {end_name} is not ready...Calling make_ready.... ")
+        end.make_ready(refresh_meta=False)
---
+++ scripts/models_ready.py
@@ -0,0 +1,23 @@
+"""Script that loops through all models and checks if they are ready"""
+import logging
+import time
+
+# SageWorks Imports
+from sageworks.views.artifacts_text_view import ArtifactsTextView
+from sageworks.artifacts.models.model import Model
+
+# Setup logging
+log = logging.getLogger("sageworks")
+
+# Create a instance of the ArtifactsTextView
+artifacts_text_view = ArtifactsTextView()
+
+# Get all the endpoints
+models = artifacts_text_view.models_summary()
+for model_name in models["Model Group"]:
+    m = Model(model_name)
+    if m.ready():
+        log.debug(f"Model {model_name} is ready!")
+    else:
+        log.important(f"Model {model_name} is not ready...Calling make_ready.... ")
+        m.make_ready(refresh_meta=False)
