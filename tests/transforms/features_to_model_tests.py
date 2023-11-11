6718350cbc7aa8d6302f441b2d55b0af1c6570a8
Brian Wylie <briford.wylie@gmail.com>
2023-11-11 08:55:44 -0700

    updating feature_to_model tests

--- tests/transforms/features_to_model_tests.py
+++ tests/transforms/features_to_model_tests.py
@@ -3,20 +3,32 @@ import pytest
 
 # Local Imports
 from sageworks.transforms.features_to_model.features_to_model import FeaturesToModel
+from sageworks.artifacts.models.model import ModelType
 
 
 # Simple test of the FeaturesToModel functionality
 @pytest.mark.slow
 def test():
-    """Tests for the Features to Model Transforms"""
+    """Test the Features to Model Transforms"""
 
     # Create the class with inputs and outputs and invoke the transform
     input_uuid = "abalone_feature_set"
     output_uuid = "abalone-regression"
-    to_model = FeaturesToModel(input_uuid, output_uuid)
+    to_model = FeaturesToModel(input_uuid, output_uuid, ModelType.REGRESSOR)
     to_model.set_output_tags(["abalone", "public"])
-    to_model.transform(target_column="class_number_of_rings")
+    to_model.transform(target_column="class_number_of_rings", description="Abalone Regression")
+
+
+def test_caps():
+    """Tests for the Features to Model Transforms"""
+
+    # Create the class with inputs and outputs and invoke the transform
+    input_uuid = "abalone_feature_set"
+    output_uuid = "Abalone-Regression-Caps"
+    to_model = FeaturesToModel(input_uuid, output_uuid, ModelType.REGRESSOR)
+    to_model.set_output_tags(["abalone", "public"])
+    to_model.transform(target_column="class_number_of_rings", description="Abalone Regression with CAPS")
 
 
 if __name__ == "__main__":
-    test()
+    test_caps()
