d649e8a25a4a369ca43ca774ac7ca6d5ee27194e
Brian Wylie <briford.wylie@gmail.com>
2023-11-12 13:53:02 -0700

    moving superclass init to the end of data_source_abstract init

--- src/sageworks/artifacts/data_sources/data_source_abstract.py
+++ src/sageworks/artifacts/data_sources/data_source_abstract.py
@@ -12,11 +12,11 @@ class DataSourceAbstract(Artifact):
     def __init__(self, uuid):
         """DataSourceAbstract: Abstract Base Class for all data sources (S3: CSV, JSONL, Parquet, RDS, etc)"""
 
-        # Call superclass init
-        super().__init__(uuid)
-
         # Set up our instance attributes
         self._display_columns = None
+
+        # Call superclass init
+        super().__init__(uuid)
 
     @abstractmethod
     def num_rows(self) -> int:
