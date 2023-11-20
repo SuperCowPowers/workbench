de8e81858a7def6231b3cd1f39f26a587fc784ff
Brian Wylie <briford.wylie@gmail.com>
2023-11-20 10:06:37 -0700

    have the example script take an argument for the endpoint name

--- examples/endpoint_inference.py
+++ examples/endpoint_inference.py
@@ -1,4 +1,5 @@
 """Example script of running inference on an Endpoint"""
+import argparse
 import pandas as pd
 
 # SageWorks Imports
@@ -6,24 +7,39 @@ from sageworks.artifacts.feature_sets.feature_set import FeatureSet
 from sageworks.artifacts.models.model import Model
 from sageworks.artifacts.endpoints.endpoint import Endpoint
 
-# Set options for actually seeing the dataframe
-pd.set_option("display.max_columns", None)
-pd.set_option("display.width", None)
 
-# Grab my Endpoint
-endpoint_name = "abalone-regression-end"
-my_endpoint = Endpoint(endpoint_name)
+def run_inference(endpoint_name):
+    # Set options for actually seeing the dataframe
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.width", None)
 
-# Grab the FeatureSet by backtracking from the Endpoint
-model = my_endpoint.get_input()
-feature_set = Model(model).get_input()
-features = FeatureSet(feature_set)
-table = features.get_training_view_table()
-test_df = features.query(f"SELECT * FROM {table} where training = 0")
+    # Grab the Endpoint
+    my_endpoint = Endpoint(endpoint_name)
 
-# Drop some columns
-test_df.drop(["write_time", "api_invocation_time", "is_deleted"], axis=1, inplace=True)
+    # Grab the FeatureSet by backtracking from the Endpoint
+    model = my_endpoint.get_input()
+    feature_set = Model(model).get_input()
+    features = FeatureSet(feature_set)
+    table = features.get_training_view_table()
+    test_df = features.query(f"SELECT * FROM {table} where training = 0")
 
-# Make predictions on the Endpoint
-pred_df = my_endpoint.predict(test_df[:10])
-print(pred_df.head())
+    # Drop some columns
+    test_df.drop(
+        ["write_time", "api_invocation_time", "is_deleted"], axis=1, inplace=True
+    )
+
+    # Make predictions on the Endpoint
+    pred_df = my_endpoint.predict(test_df[:10])
+    print(pred_df.head())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run inference on a SageWorks Endpoint"
+    )
+    parser.add_argument(
+        "endpoint_name", type=str, help="Name of the SageWorks Endpoint"
+    )
+    args = parser.parse_args()
+
+    run_inference(args.endpoint_name)
