3f0e8c57235a275f74a33c23695765d7e1a71b4f
Brian Wylie <briford.wylie@gmail.com>
2023-11-21 09:27:22 -0700

    bumping sageworks version in Dockerfile and AWS Stack

--- aws_setup/sageworks_dashboard_full/sageworks_dashboard_full/sageworks_dashboard_stack.py
+++ aws_setup/sageworks_dashboard_full/sageworks_dashboard_full/sageworks_dashboard_stack.py
@@ -101,7 +101,7 @@ class SageworksDashboardStack(Stack):
             memory_limit_mib=4096,
             cpu=2048,
         )
-        dashboard_image = "public.ecr.aws/m6i5k1r2/sageworks_dashboard:v0_2_27_amd64"
+        dashboard_image = "public.ecr.aws/m6i5k1r2/sageworks_dashboard:v0_2_28_amd64"
         container = task_definition.add_container(
             "SageworksContainer",
             image=ecs.ContainerImage.from_registry(dashboard_image),
