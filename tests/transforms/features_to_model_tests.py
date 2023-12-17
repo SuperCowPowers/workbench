674b7bb269b2f67e46ebc5d5f797146c75d51549
Brian Wylie <briford.wylie@gmail.com>
2023-12-17 09:37:56 -0700

    removing the test capitals, seems to work fine

--- tests/transforms/features_to_model_tests.py
+++ tests/transforms/features_to_model_tests.py
@@ -19,17 +19,5 @@ def test():
     to_model.transform(target_column="class_number_of_rings", description="Abalone Regression")
 
 
-@pytest.mark.long
-def test_caps():
-    """Tests for the Features to Model Transforms"""
-
-    # Create the class with inputs and outputs and invoke the transform
-    input_uuid = "abalone_feature_set"
-    output_uuid = "Abalone-Regression-Caps"
-    to_model = FeaturesToModel(input_uuid, output_uuid, ModelType.REGRESSOR)
-    to_model.set_output_tags(["abalone", "public"])
-    to_model.transform(target_column="class_number_of_rings", description="Abalone Regression with CAPS")
-
-
 if __name__ == "__main__":
-    test_caps()
+    test()
