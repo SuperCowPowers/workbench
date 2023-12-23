d1282b5c7d81e90a16dee7f5d8cae0c5633703d8
Brian Wylie <briford.wylie@gmail.com>
2023-12-23 14:15:57 -0700

    adding support for DataFrame construction and adding the to_features() functionality

--- src/sageworks/api/data_source.py
+++ src/sageworks/api/data_source.py
@@ -1,11 +1,15 @@
 """DataSource: SageWorks DataSource API Class"""
 import os
+import pandas as pd
 import logging
 
 # SageWorks Imports
 from sageworks.core.artifacts.athena_source import AthenaSource
 from sageworks.core.transforms.data_loaders.light.csv_to_data_source import CSVToDataSource
 from sageworks.core.transforms.data_loaders.light.s3_to_data_source_light import S3ToDataSourceLight
+from sageworks.core.transforms.pandas_transforms.pandas_to_data import PandasToData
+from sageworks.core.transforms.data_to_features.light.data_to_features_light import DataToFeaturesLight
+from sageworks.api.feature_set import FeatureSet
 from sageworks.utils.aws_utils import extract_data_source_basename
 
 
@@ -19,10 +23,10 @@ class DataSource(AthenaSource):
         my_data.to_features()
     """
 
-    def __init__(self, source, name=None, tags: list = None):
+    def __init__(self, source, name: str = None, tags: list = None):
         """DataSource Initialization
         Args:
-            source (str): The source of the data (S3, File, or Existing DataSource)
+            source (str): The source of the data (S3, File, Dataframe or Existing DataSource)
             name (str): Set the name of the data source (optional)
             tags (list): Set the tags for the data source (optional)
         """
@@ -30,15 +34,49 @@ class DataSource(AthenaSource):
 
         # Load the source (S3, File, or Existing DataSource)
         ds_name = extract_data_source_basename(source) if name is None else name
+        if ds_name == "dataframe":
+            msg = "Set the 'name' argument in the constructor: DataSource(df, name='my_data')"
+            self.log.critical(msg)
+            raise ValueError(msg)
         tags = [ds_name] if tags is None else tags
         self._load_source(source, ds_name, tags)
 
         # Call superclass init
         super().__init__(ds_name)
 
+    def to_features(self, name: str = None, tags: list = None, id_column: str = None, event_time_column: str = None) -> FeatureSet:
+        """Convert the DataSource to a FeatureSet
+        Args:
+            name (str): Set the name for feature set (optional)
+            tags (list): Set the tags for the feature set (optional)
+            id_column (str): Set the id column for the feature set (optional)
+            event_time_column (str): Set the event time column for the feature set (optional)
+        Returns:
+            FeatureSet: The FeatureSet created from the DataSource
+        """
+
+        # Create the FeatureSet Name and Tags
+        fs_name = self.uuid.replace("_data", "") + "_features" if name is None else name
+        tags = [fs_name] if tags is None else tags
+
+        # Transform the DataSource to a FeatureSet
+        data_to_features = DataToFeaturesLight(self.uuid, fs_name)
+        data_to_features.set_output_tags(tags)
+        data_to_features.transform(id_column=id_column, event_time_column=event_time_column)
+
+        # Return the FeatureSet (which will now be up-to-date)
+        return FeatureSet(fs_name)
+
     def _load_source(self, source: str, name: str, tags: list):
         """Load the source of the data"""
         self.log.info(f"Loading source: {source}...")
+
+        # Pandas DataFrame Source
+        if isinstance(source, pd.DataFrame):
+            my_loader = PandasToData(name)
+            my_loader.set_input(source)
+            my_loader.set_output_tags(tags)
+            my_loader.transform()
 
         # S3 Source
         source = source if isinstance(source, str) else str(source)
@@ -59,19 +97,40 @@ if __name__ == "__main__":
     import sys
     from pathlib import Path
     from pprint import pprint
+    from sageworks.utils.test_data_generator import TestDataGenerator
+
+    # Test to Run
+    long_tests = False
 
     # Retrieve an existing Data Source
-    my_data = DataSource("test_data")
-    pprint(my_data.summary())
-    pprint(my_data.details())
+    test_data = DataSource("test_data")
+    pprint(test_data.summary())
+    pprint(test_data.details())
 
-    # Create a new Data Source from a CSV file
-    abalone_data_path = Path(sys.modules["sageworks"].__file__).parent.parent.parent / "data" / "abalone.csv"
-    my_data = DataSource(abalone_data_path)
-
-    # Create a new Data Source from an S3 Path
-    my_data = DataSource("s3://sageworks-public-data/common/abalone.csv")
+    # Create a new Data Source from a Pandas DataFrame
+    gen_data = TestDataGenerator()
+    df = gen_data.person_data()
+    test_data = DataSource(df, name="test_data")
+    pprint(test_data.summary())
 
     # Example of using the Parent Class/Core API for lower level access
     my_data = DataSource("abalone_data")
-    print(my_data.outliers().head())
+    pprint(my_data.outliers().head())
+
+    # Long Tests
+    if long_tests:
+        # Create a new Data Source from a CSV file
+        abalone_data_path = Path(sys.modules["sageworks"].__file__).parent.parent.parent / "data" / "abalone.csv"
+        my_data = DataSource(abalone_data_path)
+        pprint(my_data.summary())
+        pprint(my_data.details())
+
+        # Create a new Data Source from an S3 Path
+        my_data = DataSource("s3://sageworks-public-data/common/abalone.csv")
+        pprint(my_data.summary())
+        pprint(my_data.details())
+
+        # Convert the Data Source to a Feature Set
+        my_features = test_data.to_features()
+        pprint(my_features.summary())
+        pprint(my_features.details())
