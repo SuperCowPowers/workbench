b1bc95056322ad78fc98285cbb304829973460f3
Brian Wylie <briford.wylie@gmail.com>
2023-12-28 14:41:54 -0700

    adding the featureset to model example

---
+++ examples/featureset_to_model.py
@@ -0,0 +1,11 @@
+from sageworks.api.feature_set import FeatureSet
+from sageworks.api.model import ModelType
+from pprint import pprint
+
+# Grab a FeatureSet
+my_features = FeatureSet('test_features')
+
+# Create a Model from the FeatureSet
+# Note: For categorical models use ModelType.CATEGORICAL
+my_model = my_features.to_model(model_type=ModelType.REGRESSOR, target_column="iq_score")
+pprint(my_model.details())
