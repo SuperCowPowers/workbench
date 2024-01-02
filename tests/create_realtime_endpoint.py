6b1b5e2714a66464af592d131c6058aa83e6c1f9
Brian Wylie <briford.wylie@gmail.com>
2024-01-02 16:01:47 -0700

    updating the create realtime test artifact script

--- tests/create_realtime_endpoint.py
+++ tests/create_realtime_endpoint.py
@@ -6,22 +6,23 @@ Endpoints:
 import logging
 
 # Sageworks Imports
-from sageworks.core.artifacts.endpoint_core import EndpointCore
-from sageworks.core.transforms.model_to_endpoint.model_to_endpoint import ModelToEndpoint
+from sageworks.api.model import Model
+from sageworks.api.endpoint import Endpoint
 from sageworks.aws_service_broker.aws_service_broker import AWSServiceBroker
 
 # Setup the logger
 log = logging.getLogger("sageworks")
 
 if __name__ == "__main__":
-    # This forces a refresh on all the data we get from the AWs Broker
+    # This forces a refresh on all the data we get from the AWS Broker
     AWSServiceBroker().get_all_metadata(force_refresh=True)
 
     # Recreate Flag in case you want to recreate the artifacts
     recreate = False
 
     # Create the abalone_regression Endpoint
-    if recreate or not EndpointCore("abalone-regression-end-rt").exists():
-        model_to_endpoint = ModelToEndpoint("abalone-regression", "abalone-regression-end-rt", serverless=False)
-        model_to_endpoint.set_output_tags(["abalone", "regression", "realtime"])
-        model_to_endpoint.transform()
+    if recreate or not Endpoint("abalone-regression-end-rt").exists():
+        m = Model("abalone-regression")
+        m.to_endpoint("abalone-regression-end-rt",
+                      tags=["abalone", "regression", "realtime"],
+                      serverless=False)
