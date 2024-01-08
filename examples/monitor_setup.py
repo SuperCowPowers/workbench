dac1222df5ad9405250d5987b066d841338b3b1f
Brian Wylie <briford.wylie@gmail.com>
2024-01-08 11:23:51 -0700

    adding monitor examples

---
+++ examples/monitor_setup.py
@@ -0,0 +1,14 @@
+from sageworks.api.monitor import Monitor
+
+# Create an Endpoint Monitor Class and perform initial Setup
+endpoint_name = "abalone-regression-end-rt"
+mon = Monitor(endpoint_name)
+
+# Add data capture to the endpoint
+mon.add_data_capture(capture_percentage=100)
+
+# Create a baseline for monitoring
+mon.create_baseline()
+
+# Set up the monitoring schedule
+mon.create_monitoring_schedule(schedule="hourly")
