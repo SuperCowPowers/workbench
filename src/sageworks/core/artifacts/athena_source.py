457b1c861d04fa8c3113f73a16d619ed37de8790
Brian Wylie <briford.wylie@gmail.com>
2024-03-05 15:15:54 -0700

    linter formatting

--- src/sageworks/core/artifacts/athena_source.py
+++ src/sageworks/core/artifacts/athena_source.py
@@ -192,7 +192,9 @@ class AthenaSource(DataSourceAbstract):
 
     def num_rows(self) -> int:
         """Return the number of rows for this Data Source"""
-        count_df = self.query(f'select count(*) AS sageworks_count from "{self.get_database()}"."{self.get_table_name()}"')
+        count_df = self.query(
+            f'select count(*) AS sageworks_count from "{self.get_database()}"."{self.get_table_name()}"'
+        )
         return count_df["sageworks_count"][0]
 
     def num_columns(self) -> int:
