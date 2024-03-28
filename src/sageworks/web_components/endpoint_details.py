3c0a9c387ebe5854b9998fe6953e60f6b8abba7c
Brian Wylie <briford.wylie@gmail.com>
2024-03-28 11:36:56 -0600

    moving health tags into markdown_utils

--- src/sageworks/web_components/endpoint_details.py
+++ src/sageworks/web_components/endpoint_details.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output, State
 # SageWorks Imports
 from sageworks.api import Endpoint
 from sageworks.web_components.component_interface import ComponentInterface
-from sageworks.utils.symbols import health_icons
+from sageworks.utils.markdown_utils import health_tag_markdown
 
 
 class EndpointDetails(ComponentInterface):
@@ -76,7 +76,7 @@ class EndpointDetails(ComponentInterface):
 
             # Special case for the health tags
             if key == "health_tags":
-                markdown += self._health_tag_markdown(summary.get(key, []))
+                markdown += health_tag_markdown(summary.get(key, []))
                 continue
 
             # Get the value
@@ -92,33 +92,6 @@ class EndpointDetails(ComponentInterface):
             # Add to markdown string
             markdown += f"**{key}:** {value}  \n"
 
-        return markdown
-
-    @staticmethod
-    def _health_tag_markdown(health_tags: list[str]) -> str:
-        """Internal method to generate the health tag markdown
-        Args:
-            health_tags (list[str]): A list of health tags
-        Returns:
-            str: A markdown string
-        """
-        # If we have no health tags, then add a bullet for healthy
-        markdown = "**Health Checks**\n"  # Header for Health Checks
-
-        # If we have no health tags, then add a bullet for healthy
-        if not health_tags:
-            markdown += f"* Healthy: {health_icons.get('healthy')}\n\n"
-            return markdown
-
-        # Special case for no_activity with no other tags
-        if len(health_tags) == 1 and health_tags[0] == "no_activity":
-            markdown += f"* Healthy: {health_icons.get('healthy')}\n"
-            markdown += f"* No Activity: {health_icons.get('no_activity')}\n\n"
-            return markdown
-
-        # If we have health tags, then add a bullet for each tag
-        markdown += "\n".join(f"* {tag}: {health_icons.get(tag, '')}" for tag in health_tags)
-        markdown += "\n\n"  # Add newlines for separation
         return markdown
 
 
--- src/sageworks/web_components/model_details.py
+++ src/sageworks/web_components/model_details.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output, State
 # SageWorks Imports
 from sageworks.api import Model
 from sageworks.web_components.component_interface import ComponentInterface
-from sageworks.utils.symbols import health_icons
+from sageworks.utils.markdown_utils import health_tag_markdown
 
 
 class ModelDetails(ComponentInterface):
@@ -107,7 +107,7 @@ class ModelDetails(ComponentInterface):
 
             # Special case for the health tags
             if key == "health_tags":
-                markdown += self._health_tag_markdown(summary.get(key, []))
+                markdown += health_tag_markdown(summary.get(key, []))
                 continue
 
             # Special case for the features
@@ -194,33 +194,6 @@ class ModelDetails(ComponentInterface):
 
         # Return the options for the dropdown and the selected value
         return inference_runs, default_inference_run
-
-    @staticmethod
-    def _health_tag_markdown(health_tags: list[str]) -> str:
-        """Internal method to generate the health tag markdown
-        Args:
-            health_tags (list[str]): A list of health tags
-        Returns:
-            str: A markdown string
-        """
-        # If we have no health tags, then add a bullet for healthy
-        markdown = "**Health Checks**\n"  # Header for Health Checks
-
-        # If we have no health tags, then add a bullet for healthy
-        if not health_tags:
-            markdown += f"* Healthy: {health_icons.get('healthy')}\n\n"
-            return markdown
-
-        # Special case for no_activity with no other tags
-        if len(health_tags) == 1 and health_tags[0] == "no_activity":
-            markdown += f"* Healthy: {health_icons.get('healthy')}\n"
-            markdown += f"* No Activity: {health_icons.get('no_activity')}\n\n"
-            return markdown
-
-        # If we have health tags, then add a bullet for each tag
-        markdown += "\n".join(f"* {tag}: {health_icons.get(tag, '')}" for tag in health_tags)
-        markdown += "\n\n"  # Add newlines for separation
-        return markdown
 
 
 if __name__ == "__main__":
