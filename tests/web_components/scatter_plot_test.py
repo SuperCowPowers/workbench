1773f6d8352419f7a504b4756d4e5cd152ed46ac
Brian Wylie <briford.wylie@gmail.com>
2024-06-13 16:18:38 -0600

    adding a scatter plot test

---
+++ tests/web_components/scatter_plot_test.py
@@ -0,0 +1,25 @@
+"""Tests for graph_plot web component"""
+
+# SageWorks Imports
+from sageworks.api import FeatureSet
+from sageworks.web_components.plugins.scatter_plot import ScatterPlot
+
+
+def test_scatter_plot():
+    """Test the ScatterPlot class"""
+
+    # Instantiate a Graph
+    fs = FeatureSet("abalone_features")
+
+    # Instantiate the ScatterPlot class
+    plot = ScatterPlot()
+
+    # Generate the figure (it's the first element in the property list)
+    all_properties = plot.update_properties(fs)
+    figure = all_properties[0]
+    figure.show()
+
+
+if __name__ == "__main__":
+    # Run the tests
+    test_scatter_plot()
