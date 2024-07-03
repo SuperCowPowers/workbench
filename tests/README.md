2dc52125455aeed395951de8531ec359cc2a2d6d
Brian Wylie <briford.wylie@gmail.com>
2024-07-03 17:13:59 -0600

    adding a readme and a script for creating the quantile regression artifacts

---
+++ tests/README.md
@@ -0,0 +1,22 @@
+
+# SageWorks Tests
+**Important:** In order to run the tests you'll need to run the scripts in the `/create_test_artifacts` directory to create the AWS ML Pipeline objects that get tested. 
+
+At a minimum run these two:
+
+```
+cd sageworks/test/create_test_artifacts
+python create_basic_test_artifacts.py
+python create_wine_artifacts.py
+```
+
+## Running the tests
+Okay the testing ML Pipeline object only need to be created once, so after that you can run the SageWorks test suite. In the top level directory of the SageWorks repository you can simply run
+
+```
+tox
+```
+
+### Questions?
+The SuperCowPowers team is happy to anser any questions you may have about AWS and SageWorks. Please contact us at [sageworks@supercowpowers.com](mailto:sageworks@supercowpowers.com) or on chat us up on [Discord](https://discord.gg/WHAJuz8sw8) 
+<img align="right" src="../docs/images/scp.png" width="180">
---
+++ tests/create_test_artifacts/create_quant_regression_artifacts.py
@@ -0,0 +1,47 @@
+"""This Script creates the Abalone Quantile Regression Artifacts in AWS/SageWorks
+
+Models:
+    - abalone-quantile-reg
+Endpoints:
+    - abalone-qr-end
+"""
+
+import logging
+import pandas as pd
+import awswrangler as wr
+
+from sageworks.api.data_source import DataSource
+from sageworks.api.feature_set import FeatureSet
+from sageworks.api.model import Model, ModelType
+from sageworks.api.endpoint import Endpoint
+
+from sageworks.core.transforms.data_to_features.light.molecular_descriptors import MolecularDescriptors
+from sageworks.aws_service_broker.aws_service_broker import AWSServiceBroker
+
+log = logging.getLogger("sageworks")
+
+
+if __name__ == "__main__":
+    # This forces a refresh on all the data we get from the AWs Broker
+    AWSServiceBroker().get_all_metadata(force_refresh=True)
+
+    # Recreate Flag in case you want to recreate the artifacts
+    recreate = False
+
+    # Create the Abalone Quantile Regression Model
+    if recreate or not Model("abalone-quantile-reg").exists():
+
+        # Transform FeatureSet into Quantile Regression Model
+        feature_set = FeatureSet("abalone_features")
+        feature_set.to_model(
+            ModelType.REGRESSOR,
+            target_column="class_number_of_rings",
+            name="abalone-quantile-reg",
+            description="Abalone Quantile Regression",
+            tags=["abalone", "quantiles"],
+        )
+
+    # Create the Abalone Quantile Regression Endpoint
+    if recreate or not Endpoint("abalone-qr-end").exists():
+        m = Model("abalone-quantile-reg")
+        m.to_endpoint(name="abalone-qr-end", tags=["abalone", "quantiles"])
