5c2fa3771357f11ad52de2485e4f0d5eab4c6833
Brian Wylie <briford.wylie@gmail.com>
2024-07-11 16:57:04 -0600

    just a bunch of testing for new quantiles

--- src/sageworks/web_components/plugins/scatter_plot.py
+++ src/sageworks/web_components/plugins/scatter_plot.py
@@ -258,14 +258,22 @@ if __name__ == "__main__":
     end = Endpoint("aqsol-qr-end")
 
     # Domain specific error_distance (if we got it wrong by this much, we have low confidence)
-    error_distance = 1.0
+    error_distance = 1.5
 
-    fs_data = backtrack_to_fs(end).pull_dataframe()
-    pred_df = end.inference(fs_data)
+    # Run inference on the endpoint (only need to do this if the model has changed)
+    # fs_data = backtrack_to_fs(end).pull_dataframe()
+    # end.inference(fs_data, capture_uuid="qrr_2024_07_11", id_column="id")
+
+    # Get the endpoint model and target column
+    model = Model(end.get_input())
+    target = model.target()
+
+    # Grab the inference data
+    pred_df = model.get_inference_predictions("qrr_2024_07_11")
 
     # Domain specific confidence
     pred_df["target_spread"] = pred_df["q_95"] - pred_df["q_05"]
-    pred_df["target_confidence"] = np.clip(1 - (pred_df["target_spread"] / error_distance), 0, 1)
+    pred_df["target_confidence"] = np.clip(1 - (pred_df["target_spread"] / (error_distance*4.0)), 0, 1)
 
     # - any interval greater than 2.0 we have no confidence
     # - anything less than 2.0 is a linear gradient from 0.0 to 1.0
@@ -286,22 +294,20 @@ if __name__ == "__main__":
 
     # Compute the median delta for the prediction
     pred_df["median_delta"] = np.abs(pred_df["q_50"] - pred_df["prediction"])
-    pred_df["median_confidence"] = np.clip(1 - (pred_df["median_delta"] / (error_distance*2)), 0, 1)
+    pred_df["median_confidence"] = np.clip(1 - (pred_df["median_delta"] / (error_distance/2)), 0, 1)
 
     # Confidence is the product of target, residual, and median confidence
     # pred_df["confidence"] = pred_df["target_confidence"] * pred_df["residual_confidence"] * pred_df["median_confidence"]
-    pred_df["confidence"] = pred_df["residual_confidence"] * pred_df["median_confidence"]
-
-    # Get the endpoint model and target column
-    model = Model(end.get_input())
-    target = model.target()
+    # pred_df["confidence"] = pred_df["residual_confidence"] * pred_df["median_confidence"]
+    # pred_df["confidence"] = pred_df["residual_confidence"] * pred_df["target_confidence"]
+    pred_df["confidence"] = pred_df["residual_confidence"]
 
     # Grab the performance metrics
     metrics = model.get_inference_metrics()
     pprint(metrics)
 
     # Confidence Threshold
-    confidence_thres = 0.2
+    confidence_thres = 0.5
 
     # Now filter the data based on confidence and give RMSE for the filtered data
     predictions_filtered = pred_df[pred_df["confidence"] > confidence_thres]
@@ -312,7 +318,7 @@ if __name__ == "__main__":
     hover_columns = ["q_05", "q_25", "q_50", "q_75", "q_95",
                      "qr_05", "qr_25", "qr_50", "qr_75", "qr_95",
                      "prediction", target, "confidence",
-                     "target_spread", "residual_confidence",
+                     "target_spread", "target_confidence", "residual_confidence",
                      "median_delta", "median_confidence"]
 
-    PluginUnitTest(ScatterPlot, input_data=pred_df, hover_columns=hover_columns).run()
+    PluginUnitTest(ScatterPlot, input_data=pred_df, x="solubility", y="prediction", color="confidence", hover_columns=hover_columns).run()
