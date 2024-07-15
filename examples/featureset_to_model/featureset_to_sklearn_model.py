9fe0c199a814b4d3fd9ec343775ac7a1a17ed256
Brian Wylie <briford.wylie@gmail.com>
2024-07-15 13:02:26 -0600

    making a couple of example scripts for Scikit-Learn models

---
+++ examples/featureset_to_model/featureset_to_sklearn_model.py
@@ -0,0 +1,20 @@
+from sageworks.api.feature_set import FeatureSet
+from sageworks.api.model import ModelType
+from pprint import pprint
+
+# Grab a FeatureSet
+my_features = FeatureSet("abalone_features")
+
+# Transform FeatureSet into KNN Regression Model
+# Note: ModelTypes can be CLASSIFIER, REGRESSOR, UNSUPERVISED, or TRANSFORMER
+#       model_class can be any sckit-learn model ("KNeighborsRegressor", "BayesianRidge",
+#       "GaussianNB", "AdaBoostClassifier", "Ridge, "Lasso", "SVC", "SVR", etc...)
+my_model = my_features.to_model(
+    model_class="KNeighborsRegressor",
+    target_column="class_number_of_rings",
+    name="abalone-knn-reg",
+    description="Abalone KNN Regression",
+    tags=["abalone", "knn"],
+    train_all_data=True,
+)
+pprint(my_model.details())
