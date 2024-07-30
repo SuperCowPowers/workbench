243fdb790c5b9d804f088c74ee334e235caead1e
Brian Wylie <briford.wylie@gmail.com>
2024-07-30 11:43:45 -0600

    flake8/linter fixes

--- src/sageworks/utils/aws_utils.py
+++ src/sageworks/utils/aws_utils.py
@@ -13,7 +13,6 @@ from awswrangler.exceptions import NoFilesFound
 from pathlib import Path
 import posixpath
 from sagemaker.session import Session as SageSession
-import boto3
 from sagemaker import image_uris
 from collections.abc import Mapping, Iterable
 
@@ -23,7 +22,9 @@ log = logging.getLogger("sageworks")
 
 def get_image_uri_with_digest(framework, region, version, sm_session: SageSession):
     # Retrieve the base image URI using sagemaker SDK
-    base_image_uri = image_uris.retrieve(framework=framework, region=region, version=version, sagemaker_session=sm_session)
+    base_image_uri = image_uris.retrieve(
+        framework=framework, region=region, version=version, sagemaker_session=sm_session
+    )
     print(f"Base Image URI: {base_image_uri}")
 
     # Extract repository name and image tag from the base image URI
@@ -31,17 +32,15 @@ def get_image_uri_with_digest(framework, region, version, sm_session: SageSessio
     repository_name = repo_uri.split("/")[-1]
 
     # Use AWS CLI to get image details and find the digest
-    ecr_client = sm_session.boto_session.client('ecr', region_name=region)
+    ecr_client = sm_session.boto_session.client("ecr", region_name=region)
     response = ecr_client.describe_images(
         repositoryName=repository_name,
         imageIds=[
-            {
-                'imageTag': image_tag
-            },
+            {"imageTag": image_tag},
         ],
     )
-    if 'imageDetails' in response and len(response['imageDetails']) > 0:
-        image_digest = response['imageDetails'][0]['imageDigest']
+    if "imageDetails" in response and len(response["imageDetails"]) > 0:
+        image_digest = response["imageDetails"][0]["imageDigest"]
         full_image_uri = f"{repo_uri}@{image_digest}"
         return full_image_uri
     else:
@@ -439,9 +438,9 @@ if __name__ == "__main__":
     pprint(my_meta)
 
     # Get the image URI with digest
-    framework = 'sklearn'
-    region = 'us-west-2'
-    version = '1.2-1'
+    framework = "sklearn"
+    region = "us-west-2"
+    version = "1.2-1"
     # sm_session = SageSession()
 
     try:
