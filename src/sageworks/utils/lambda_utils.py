6047c95db006437927431c3a9ec48a9c119c9c14
Brian Wylie <briford.wylie@gmail.com>
2024-09-09 20:34:56 -0600

    flake8/linter fixes

--- src/sageworks/utils/lambda_utils.py
+++ src/sageworks/utils/lambda_utils.py
@@ -41,7 +41,7 @@ def load_lambda_layer():
 
     # Install xgboost into the extract path
     try:
-        subprocess.check_call(['pip', 'install', '--target', extract_path, 'xgboost'])
+        subprocess.check_call(["pip", "install", "--target", extract_path, "xgboost"])
         log.important("Successfully installed xgboost.")
     except subprocess.CalledProcessError as e:
         log.critical(f"Failed to install xgboost: {e}")
