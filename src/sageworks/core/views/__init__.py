472f83b47a2cea06d94bd629030633c4999c275d
Brian Wylie <briford.wylie@gmail.com>
2024-09-14 12:25:15 -0600

    improve the docstring for the views import

--- src/sageworks/core/views/__init__.py
+++ src/sageworks/core/views/__init__.py
@@ -1,15 +1,18 @@
-"""Welcome to the SageWorks View Classes
+"""
+SageWorks View Classes
 
-These class provide database views for both DataSources and Feature Sets
+This module provides various classes for creating database views for both DataSources and Feature Sets.
 
-- BaseView: A pass-through view to the data source table
-- IdentityView: Create a new view that's exactly the same as the base table
-- TrainingView: A view with an additional training columns that marks holdout ids
-- DisplayView: A view that has a subset of columns for display purposes
-- ComputationView: A view that has a subset of columns for computation purposes
-- MDQView: A view with a set of model data quality metrics
+Classes:
+    - View: Base class for representing a database view.
+    - DisplayView: Creates a view with a subset of columns for display purposes.
+    - ComputationView: Creates a view with a subset of columns for computation purposes.
+    - TrainingView: Creates a view with additional training columns for holdout IDs.
+    - MDQView: Creates a view with model data quality metrics.
+    - ColumnSubsetView: Creates a view with a specified subset of columns.
 """
 
+# Import view classes
 from .view import View
 from .display_view import DisplayView
 from .computation_view import ComputationView
@@ -17,4 +20,11 @@ from .training_view import TrainingView
 from .mdq_view import MDQView
 from .column_subset_view import ColumnSubsetView
 
-__all__ = ["View", "DisplayView", "ComputationView", "TrainingView", "MDQView", "ColumnSubsetView"]
+__all__ = [
+    "View",
+    "DisplayView",
+    "ComputationView",
+    "TrainingView",
+    "MDQView",
+    "ColumnSubsetView"
+]
