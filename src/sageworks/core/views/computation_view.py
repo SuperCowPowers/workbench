e19f5c52aef33efb304e654de987c3584c28bf66
Brian Wylie <briford.wylie@gmail.com>
2024-09-14 12:56:36 -0600

    refactoring the ComputationView class

--- src/sageworks/core/views/computation_view.py
+++ src/sageworks/core/views/computation_view.py
@@ -5,31 +5,41 @@ from typing import Union
 # SageWorks Imports
 from sageworks.api import DataSource, FeatureSet
 from sageworks.core.views.column_subset_view import ColumnSubsetView
+from sageworks.core.views.view import View
 
 
 class ComputationView(ColumnSubsetView):
-    """ComputationView Class: Create a View with a subset of columns for display purposes"""
+    """ComputationView Class: Create a View with a subset of columns for computation purposes"""
 
-    def __init__(self, artifact: Union[DataSource, FeatureSet], source_table: str = None):
-        """Initialize the ColumnSubsetView
+    @classmethod
+    def create(cls, artifact: Union[DataSource, FeatureSet], source_table: str = None,
+               column_list: Union[list[str], None] = None, column_limit: int = 30) -> Union[View, None]:
+        """Factory method to create and return a ComputationView instance.
 
         Args:
             artifact (Union[DataSource, FeatureSet]): The DataSource or FeatureSet object
             source_table (str, optional): The table/view to create the view from. Defaults to None
+            column_list (Union[list[str], None], optional): A list of columns to include. Defaults to None.
+            column_limit (int, optional): The max number of columns to include. Defaults to 30.
+
+        Returns:
+            Union[View, None]: The created View object (or None if failed to create the view)
         """
-        super().__init__("computation", artifact, source_table)
+        # Use the create logic directly from ColumnSubsetView with the "computation" view name
+        return ColumnSubsetView.create(
+            "computation", artifact, source_table, column_list, column_limit
+        )
 
 
 if __name__ == "__main__":
-    """Exercise the Training View functionality"""
+    """Exercise the Computation View functionality"""
     from sageworks.api import FeatureSet
 
     # Get the FeatureSet
     fs = FeatureSet("test_features")
 
     # Create a ComputationView
-    view_maker = ComputationView(fs)
-    compute_view = view_maker.create()
+    compute_view = ComputationView.create(fs)
 
     # Pull the computation data
     df = compute_view.pull_dataframe()
