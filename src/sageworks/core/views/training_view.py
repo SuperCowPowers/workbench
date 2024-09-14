8baf7105b43883edad6a02b08d93cef9277ccae9
Brian Wylie <briford.wylie@gmail.com>
2024-09-14 12:54:28 -0600

    refactoring the TrainingView class

--- src/sageworks/core/views/training_view.py
+++ src/sageworks/core/views/training_view.py
@@ -12,56 +12,47 @@ from sageworks.core.views.view_utils import get_column_list
 class TrainingView(CreateView):
     """TrainingView Class: A View with an additional training column that marks holdout ids"""
 
-    def __init__(self, artifact: Union[DataSource, FeatureSet], source_table: str = None):
-        """Initialize the ColumnSubsetView
+    @classmethod
+    def create(cls, feature_set: FeatureSet, source_table: str = None,
+               id_column: str = None, holdout_ids: Union[list[str], None] = None) -> Union[View, None]:
+        """Factory method to create and return a TrainingView instance.
 
         Args:
-            artifact (Union[DataSource, FeatureSet]): The DataSource or FeatureSet object
-            source_table (str, optional): The table/view to create the view from. Defaults to None
-        """
-        super().__init__("training", artifact, source_table)
-
-    def create_impl(
-        self,
-        data_source: DataSource,
-        id_column: str = None,
-        holdout_ids: Union[list[str], None] = None,
-    ) -> Union[View, None]:
-        """Create a training view that marks hold out ids
-
-        Args:
-            data_source (DataSource): The SageWorks DataSource object
-            id_column (str, optional): The name of the id column (default is None)
+            feature_set (FeatureSet): A FeatureSet object
+            source_table (str, optional): The table/view to create the view from. Defaults to None.
+            id_column (str, optional): The name of the id column. Defaults to None.
             holdout_ids (Union[list[str], None], optional): A list of holdout ids. Defaults to None.
 
         Returns:
             Union[View, None]: The created View object (or None if failed to create the view)
         """
+        # Instantiate the TrainingView with "training" as the view name
+        instance = cls("training", feature_set, source_table)
 
         # Drop any columns generated from AWS
         aws_cols = ["write_time", "api_invocation_time", "is_deleted", "event_time"]
-        source_table_columns = get_column_list(data_source, self.source_table)
+        source_table_columns = get_column_list(instance.data_source, instance.source_table)
         column_list = [col for col in source_table_columns if col not in aws_cols]
 
         # Sanity check on the id column
         if not id_column:
-            self.log.important("No id column specified, we'll try the auto_id_column ..")
-            if not self.auto_id_column:
-                self.log.error("No id column specified and no auto_id_column found, aborting ..")
+            instance.log.important("No id column specified, we'll try the auto_id_column ..")
+            if not instance.auto_id_column:
+                instance.log.error("No id column specified and no auto_id_column found, aborting ..")
                 return None
             else:
-                if self.auto_id_column not in column_list:
-                    self.log.error(f"Auto id column {self.auto_id_column} not found in column list, aborting ..")
+                if instance.auto_id_column not in column_list:
+                    instance.log.error(f"Auto id column {instance.auto_id_column} not found in column list, aborting ..")
                     return None
                 else:
-                    id_column = self.auto_id_column
+                    id_column = instance.auto_id_column
 
         # If we don't have holdout ids, create a default training view
         if not holdout_ids:
-            self._default_training_view(data_source, id_column)
-            return View(data_source, self.view_name, auto_create_view=False)
+            instance._default_training_view(instance.data_source, id_column)
+            return View(instance.data_source, instance.view_name, auto_create_view=False)
 
-        # Format the list of hold out ids for SQL IN clause
+        # Format the list of holdout ids for SQL IN clause
         if holdout_ids and all(isinstance(id, str) for id in holdout_ids):
             formatted_holdout_ids = ", ".join(f"'{id}'" for id in holdout_ids)
         else:
@@ -72,19 +63,19 @@ class TrainingView(CreateView):
 
         # Construct the CREATE VIEW query
         create_view_query = f"""
-        CREATE OR REPLACE VIEW {self.table} AS
+        CREATE OR REPLACE VIEW {instance.table} AS
         SELECT {sql_columns}, CASE
             WHEN {id_column} IN ({formatted_holdout_ids}) THEN 0
             ELSE 1
         END AS training
-        FROM {self.source_table}
+        FROM {instance.source_table}
         """
 
         # Execute the CREATE VIEW query
-        data_source.execute_statement(create_view_query)
+        instance.data_source.execute_statement(create_view_query)
 
         # Return the View
-        return View(data_source, self.view_name, auto_create_view=False)
+        return View(instance.data_source, instance.view_name, auto_create_view=False)
 
     # This is an internal method that's used to create a default training view
     def _default_training_view(self, data_source: DataSource, id_column: str):
@@ -103,8 +94,7 @@ class TrainingView(CreateView):
         # Enclose each column name in double quotes
         sql_columns = ", ".join([f'"{column}"' for column in column_list])
 
-        #    Construct the CREATE VIEW query with a simple modulo operation for the 80/20 split
-        #    using the id column as the stable identifier for row numbering
+        # Construct the CREATE VIEW query with a simple modulo operation for the 80/20 split
         create_view_query = f"""
         CREATE OR REPLACE VIEW {self.table} AS
         SELECT {sql_columns}, CASE
@@ -126,12 +116,11 @@ if __name__ == "__main__":
     fs = FeatureSet("test_features")
 
     # Delete the existing training view
-    training_view = TrainingView(fs).create()
+    training_view = TrainingView.create(fs)
     training_view.delete()
 
     # Create a default TrainingView
-    make_view = TrainingView(fs)
-    training_view = make_view.create()
+    training_view = TrainingView.create(fs)
     print(training_view)
 
     # Pull the training data
@@ -141,7 +130,7 @@ if __name__ == "__main__":
 
     # Create a TrainingView with holdout ids
     my_holdout_ids = list(range(10))
-    training_view = make_view.create(id_column="id", holdout_ids=my_holdout_ids)
+    training_view = TrainingView.create(fs, id_column="id", holdout_ids=my_holdout_ids)
 
     # Pull the training data
     df = training_view.pull_dataframe()
