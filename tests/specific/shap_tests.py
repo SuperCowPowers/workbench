6f86af397ee7e2c552c53d1bae193b8706a229b9
Brian Wylie <briford.wylie@gmail.com>
2024-09-25 16:07:36 -0600

    adding some shap tests

---
+++ tests/specific/shap_tests.py
@@ -0,0 +1,31 @@
+"""Tests for the SHAP Feature Importance functionality"""
+
+# Local Imports
+from sageworks.api import Endpoint
+
+
+def test_generate_shap_values_reg():
+    """Test the generation of SHAP values for a regression model"""
+
+    # Grab a test regression endpoint
+    end = Endpoint( "abalone-regression-end")
+
+    # Running auto_inference with capture=True will invoke the SHAP values generation
+    print("Running SHAP values generation for regression")
+    end.auto_inference(capture=True)
+
+
+def test_generate_shap_values_class():
+    """Test the generation of SHAP values for a classification model"""
+
+    # Grab a test classification endpoint
+    end = Endpoint("wine-classification-end")
+
+    # Running auto_inference with capture=True will invoke the SHAP values generation
+    print("Running SHAP values generation for classification")
+    end.auto_inference(capture=True)
+
+
+if __name__ == "__main__":
+    test_generate_shap_values_reg()
+    test_generate_shap_values_class()
