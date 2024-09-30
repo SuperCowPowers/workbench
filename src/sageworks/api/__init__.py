d3002b330e3b47154bdefc2b2e4d5d7615c0c525
Brian Wylie <briford.wylie@gmail.com>
2024-09-30 11:36:00 -0600

    adding matplotlib to the ui dependencies

--- src/sageworks/api/__init__.py
+++ src/sageworks/api/__init__.py
@@ -15,5 +15,6 @@ from .model import Model, ModelType
 from .endpoint import Endpoint
 from .meta import Meta
 from .parameter_store import ParameterStore
+from .df_store import DFStore
 
-__all__ = ["DataSource", "FeatureSet", "Model", "ModelType", "Endpoint", "Meta", "ParameterStore"]
+__all__ = ["DataSource", "FeatureSet", "Model", "ModelType", "Endpoint", "Meta", "ParameterStore", "DFStore"]
