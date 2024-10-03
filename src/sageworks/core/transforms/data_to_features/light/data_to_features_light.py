3d271aedf071889a43c8a5a80828b741d7ecea36
Brian Wylie <briford.wylie@gmail.com>
2024-10-03 10:52:28 -0600

    flake8/linter cleanup

--- src/sageworks/core/transforms/data_to_features/light/data_to_features_light.py
+++ src/sageworks/core/transforms/data_to_features/light/data_to_features_light.py
@@ -61,7 +61,9 @@ class DataToFeaturesLight(Transform):
         """
         # Now publish to the output location
         output_features = PandasToFeatures(self.output_uuid)
-        output_features.set_input(self.output_df, id_column=id_column, event_time_column=event_time_column, one_hot_columns=one_hot_columns)
+        output_features.set_input(
+            self.output_df, id_column=id_column, event_time_column=event_time_column, one_hot_columns=one_hot_columns
+        )
         output_features.set_output_tags(self.output_tags)
         output_features.add_output_meta(self.output_meta)
         output_features.transform()
