e25a97b501ce8ce7555b91a79dda6f2a9bc8c68b
Brian Wylie <briford.wylie@gmail.com>
2024-10-15 14:13:56 -0600

    adding a test for column changes

---
+++ tests/specific/column_changes.py
@@ -0,0 +1,21 @@
+
+import awswrangler as wr
+from sageworks.api.data_source import DataSource
+from pprint import pprint
+
+
+def column_changes_test():
+    # Create a new Data Source from an S3 Path
+    source_path = "s3://sageworks-public-data/common/abalone.csv"
+    df = wr.s3.read_csv(source_path)
+    my_data = DataSource(df, "test_columns")
+    pprint(my_data.summary())
+
+    # Change the columns of the Data Source and create DataSource with same name
+    df.rename(columns={"diameter": "foo"}, inplace=True)
+    my_data = DataSource(df, "test_columns")
+    pprint(my_data.summary())
+
+
+if __name__ == "__main__":
+    column_changes_test()
