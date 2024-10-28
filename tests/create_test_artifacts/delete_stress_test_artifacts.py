42d2f2bcbc3877618c9b6aaac4ba472d32321b15
Brian Wylie <briford.wylie@gmail.com>
2024-10-27 19:40:37 -0600

    cleaning up the delete script for stress tests

--- tests/create_test_artifacts/delete_stress_test_artifacts.py
+++ tests/create_test_artifacts/delete_stress_test_artifacts.py
@@ -1,20 +1,22 @@
-"""This Script Deletes the SageWorks Artifacts used for the tests"""
+"""This Script Deletes the SageWorks Artifacts used for stress tests"""
 
 import time
-from sageworks.api.feature_set import FeatureSet
-from sageworks.api.model import Model
-from sageworks.api.endpoint import Endpoint
-from sageworks.aws_service_broker.aws_service_broker import AWSServiceBroker
+from sageworks.api import DataSource, FeatureSet, Model, Endpoint, Meta
 
 
 if __name__ == "__main__":
     # This forces a refresh on all the data we get from the AWs Broker
-    AWSServiceBroker().get_all_metadata(force_refresh=True)
+    Meta().refresh_all_aws_meta()
 
     # Generated Model Names
-    model_names = [f"test-model-{i}" for i in range(16)]
-    feature_names = [f"{model.replace('-', '_')}_features" for model in model_names]
+    model_names = [f"stress-model-{i}" for i in range(16)]
+    datasource_names = [f"{model.replace('-model-', '_data_')}" for model in model_names]
+    feature_names = [f"{model.replace('-model-', '_features_')}" for model in model_names]
     endpoint_names = [f"{model}-end" for model in model_names]
+
+    # Delete the DataSources
+    for feature_name in feature_names:
+        DataSource.delete(feature_name)
 
     # Delete the FeatureSets
     for feature_name in feature_names:
