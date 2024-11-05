b5a8f1e74160aa881180cb63c5b288106ba815df
Brian Wylie <briford.wylie@gmail.com>
2024-11-05 13:19:38 -0700

    updating common usage doc strings

--- src/sageworks/core/transforms/features_to_model/features_to_model.py
+++ src/sageworks/core/transforms/features_to_model/features_to_model.py
@@ -22,7 +22,7 @@ class FeaturesToModel(Transform):
         to_model = FeaturesToModel(feature_uuid, model_uuid, model_type=ModelType)
         to_model.set_output_tags(["abalone", "public", "whatever"])
         to_model.transform(target_column="class_number_of_rings",
-                           input_feature_list=[feature_list])
+                           feature_list=["my", "best", "features"])
         ```
     """
 
