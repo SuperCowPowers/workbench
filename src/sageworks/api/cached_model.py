eeec59bed09b647145d2ad6b1665e4963a1b1d20
Brian Wylie <briford.wylie@gmail.com>
2024-11-11 19:16:03 -0700

    caching a few more methods

--- src/sageworks/api/cached_model.py
+++ src/sageworks/api/cached_model.py
@@ -1,4 +1,5 @@
 """CachedModel: Caches the method results for SageWorks Models"""
+from typing import Union
 
 # SageWorks Imports
 from sageworks.core.artifacts.model_core import ModelCore
@@ -16,7 +17,9 @@ class CachedModel(CachedArtifactMixin, ModelCore):
         ```
     """
     def __init__(self, uuid: str):
-        super().__init__(uuid)  # Use super() to call both mixin and base class initializers
+        """CachedModel Initialization"""
+        # Call the initializer of ModelCore, passing the uuid argument
+        ModelCore.__init__(self, model_uuid=uuid)
 
     @CachedArtifactMixin.cache_result
     def summary(self, **kwargs) -> dict:
@@ -45,6 +48,24 @@ class CachedModel(CachedArtifactMixin, ModelCore):
         """
         return super().health_check(**kwargs)
 
+    @CachedArtifactMixin.cache_result
+    def get_endpoint_inference_path(self) -> Union[str, None]:
+        """Retrieve the Endpoint Inference Path.
+
+        Returns:
+            str: The Endpoint Inference Path
+        """
+        return super().get_endpoint_inference_path()
+
+    @CachedArtifactMixin.cache_result
+    def sageworks_meta(self) -> Union[str, None]:
+        """Retrieve the Enumerated Model Type (REGRESSOR, CLASSIFER, etc).
+
+        Returns:
+            str: The Enumerated Model Type
+        """
+        return super().sageworks_meta()
+
 
 if __name__ == "__main__":
     """Exercise the CachedModel Class"""
@@ -55,4 +76,5 @@ if __name__ == "__main__":
     pprint(my_model.summary())
     pprint(my_model.details())
     pprint(my_model.health_check())
+    print(my_model.get_endpoint_inference_path())
     my_model.close()
