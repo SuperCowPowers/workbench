b15625fb93e86a74bb2ae08643c4f1520815554e
Brian Wylie <briford.wylie@gmail.com>
2024-11-11 20:00:36 -0700

    putting cache spindown sleep in the timing script

--- scripts/instantiation_timings.py
+++ scripts/instantiation_timings.py
@@ -8,3 +8,7 @@ for model_name in models["Model Group"]:
     start_time = time.time()
     model = CachedModel(model_name)
     print(f"{model_name} instantiation Time: {time.time() - start_time:.3f} seconds")
+
+# Sleep to let's Cache Threads spin down
+print("Cache spindown...")
+time.sleep(5)
