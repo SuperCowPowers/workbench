fdaa0f71f36c5b6036163cd08de36df7c29cf5bb
Brian Wylie <briford.wylie@gmail.com>
2024-11-14 10:36:46 -0700

    having the instantiation script loop over the existing models

--- scripts/instantiation_timings.py
+++ scripts/instantiation_timings.py
@@ -1,10 +1,21 @@
 import time
 from sageworks.cached.cached_meta import CachedMeta
 from sageworks.cached.cached_model import CachedModel
+from sageworks.api import Model
 
 # Instantiate all the Model objects
-models = CachedMeta().models()
-for model_name in models["Model Group"]:
+models = CachedMeta().models()["Model Group"].tolist()
+models = models[:10]  # Ten for testing
+
+# Normal Models
+print("Model()")
+for model_name in models:
+    start_time = time.time()
+    model = Model(model_name)
+    print(f"{model_name} instantiation Time: {time.time() - start_time:.3f} seconds")
+
+print("CachedModel()")
+for model_name in models:
     start_time = time.time()
     model = CachedModel(model_name)
     print(f"{model_name} instantiation Time: {time.time() - start_time:.3f} seconds")
