bd825ab442d78cc17cd9b885db16d68fc540d518
Brian Wylie <briford.wylie@gmail.com>
2024-11-18 14:44:00 -0700

    fix tab whoops

--- src/sageworks/core/transforms/features_to_model/features_to_model.py
+++ src/sageworks/core/transforms/features_to_model/features_to_model.py
@@ -371,6 +371,6 @@ if __name__ == "__main__":
     my_script = scripts_root / "custom_models" / "chem_info" / "morgan_fingerprints.py"
     input_uuid = "aqsol_features"
     output_uuid = "smiles-to-fingerprints-v1"
-to_model = FeaturesToModel(input_uuid, output_uuid, model_type=ModelType.TRANSFORMER, custom_script=my_script)
-to_model.set_output_tags(["smiles", "morgan fingerprints"])
-to_model.transform(target_column=None, feature_list=["smiles"], description="Smiles to Morgan Fingerprints")
+    to_model = FeaturesToModel(input_uuid, output_uuid, model_type=ModelType.TRANSFORMER, custom_script=my_script)
+    to_model.set_output_tags(["smiles", "morgan fingerprints"])
+    to_model.transform(target_column=None, feature_list=["smiles"], description="Smiles to Morgan Fingerprints")
