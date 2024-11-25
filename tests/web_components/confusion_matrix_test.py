932942955151f9b2a31935c13d8bcb9fcfccecfc
Brian Wylie <briford.wylie@gmail.com>
2024-11-24 19:38:03 -0700

    fixing confusion matrix test

--- tests/web_components/confusion_matrix_test.py
+++ tests/web_components/confusion_matrix_test.py
@@ -1,27 +1,24 @@
 """Tests for confusion_matrix web component"""
 
 # SageWorks Imports
-from sageworks.web_components.confusion_matrix import ConfusionMatrix
+from sageworks.web_components.plugins.confusion_matrix import ConfusionMatrix
 from sageworks.api.model import Model
 
 
 def test_confusion_matrix():
     """Test the ConfusionMatrix class"""
+
     # Instantiate model
-    m = Model("wine-classification")
+    model = Model("wine-classification")
     inference_run = "auto_inference"
 
     # Instantiate the ConfusionMatrix class
     cm = ConfusionMatrix()
 
-    # Generate the figure
-    fig = cm.update_properties(m, inference_run=inference_run)
-
-    # Apply dark theme
-    fig.update_layout(template="plotly_dark")
-
-    # Show the figure
-    fig.show()
+    # Update the properties (the figure is the first element in the property list)
+    all_properties = cm.update_properties(model, inference_run=inference_run)
+    figure = all_properties[0]
+    figure.show()
 
 
 if __name__ == "__main__":
