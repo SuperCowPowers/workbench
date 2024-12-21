987b7181b211f7fa870acdbc2b504f8690f9cd8f
Brian Wylie <briford.wylie@gmail.com>
2024-12-20 19:59:34 -0700

    just some log/output changes

--- aws_setup/aws_account_check.py
+++ aws_setup/aws_account_check.py
@@ -36,6 +36,7 @@ class AWSAccountCheck:
             wr.catalog.create_database(catalog_db, exist_ok=True, boto3_session=self.aws_clamp.boto3_session)
         except ClientError as e:
             if e.response["Error"]["Code"] == "AccessDeniedException":
+                self.log.error(f"AccessDeniedException {e}")
                 self.log.error(f"Access denied while trying to create/access the catalog database '{catalog_db}'.")
                 self.log.error("Create the database manually in the AWS Glue Console, or run this command:")
                 self.log.error('aws glue create-database --database-input \'{"Name": "workbench"}\'')
@@ -75,14 +76,18 @@ class AWSAccountCheck:
                 self.log.info(f"The {folder} prefix does not exist...which is fine...")
 
     def check(self):
-        """Check if the AWS Account is Setup Correctly"""
-        self.log.info("*** AWS Identity Check ***")
+        """Check if the AWS Account is set up Correctly"""
+        self.log.info("*** Caller/Base AWS Identity Check ***")
         self.aws_clamp.check_aws_identity()
-        self.log.info("Identity Check Success...")
+        print("\n")
+
+        self.log.info("*** AWS Assumed Role Check ***")
+        self.aws_clamp.check_assumed_role()
+        print("\n")
 
         self.log.info("*** AWS S3 Access Check ***")
         self.aws_clamp.check_s3_access()
-        self.log.info("S3 Access Check Success...")
+        print("\n")
 
         # Check that the Workbench S3 Bucket and Sub-folders are created
         self.check_s3_bucket_subfolders()
@@ -91,20 +96,22 @@ class AWSAccountCheck:
         sm_client = self.aws_clamp.sagemaker_client()
         for feature_group in sm_client.list_feature_groups()["FeatureGroupSummaries"]:
             self.log.info(str(feature_group))
+        print("\n")
 
         # Check that the Glue Database exist
         self.log.info("*** AWS Glue Database Check ***")
         for catalog_db in ["workbench", "sagemaker_featurestore"]:
             self.ensure_aws_catalog_db(catalog_db)
+        print("\n")
 
-        self.log.info("\n\nAWS Account Clamp: AOK!")
+        self.log.info("AWS Account Clamp: AOK!")
 
 
 if __name__ == "__main__":
     """Exercise the AWS Account Clamp Class"""
 
     # Create the class
-    aws_clamp = AWSAccountCheck()
+    account_check = AWSAccountCheck()
 
     # Check that out AWS Account Clamp is working AOK
-    aws_clamp.check()
+    account_check.check()
