b1c1fb09b05aacc0f613942e636f20c4a620e740
Brian Wylie <briford.wylie@gmail.com>
2025-01-06 10:39:57 -0700

    adding compound_explorer AWS stack

---
+++ aws_setup/compound_explorer/README.md
@@ -0,0 +1,37 @@
+
+# Compound Explorer
+This is the Python CDK for deploying the Compound Explorer web service AWS Stack. The AWS Stack has quite a few components.
+
+- ECS Fargate: Deploying a Fargate service for running containerized applications.
+- Application Load Balancer: Configured to balance traffic to the Fargate service, with optional public access and HTTPS support.
+- VPC: A Virtual Private Cloud with subnets spread across 2 Availability Zones for high availability. The VPC includes:
+  - Public Subnets: For resources that need to be accessible from the internet, such as the Load Balancer when in public mode.
+  - Private Subnets: For resources that should not be directly accessible from the internet, such as the ECS tasks and the Redis cluster.
+- NAT Gateways: Deployed in each public subnet to allow outbound internet access for resources in the private subnets.
+- ElastiCache Redis Cluster: A Redis cluster in Amazon ElastiCache, deployed in the private subnets for caching purposes, accessible only from within the VPC.
+- Security Groups: Configured to control access to the ECS tasks and the Redis cluster, with rules based on IP whitelisting and AWS managed prefix lists.
+
+
+## Setting WORKBENCH_CONFIG before Synth/Diff/Deploy
+```
+export WORKBENCH_CONFIG=/full/path/to/deploy_config.json
+```
+At this point you can now synthesize the CloudFormation template for this code.
+
+## Synth, Diff, and Deploy
+```
+$ cdk synth
+$ cdk diff
+$ cdk deploy
+```
+
+
+## Useful commands
+
+ * `cdk ls`          list all stacks in the app
+ * `cdk synth`       emits the synthesized CloudFormation template
+ * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `cdk diff`        compare deployed stack with current state
+ * `cdk docs`        open CDK documentation
+
+Enjoy!
---
+++ aws_setup/compound_explorer/app.py
@@ -0,0 +1,60 @@
+import boto3
+import aws_cdk as cdk
+from pprint import pprint
+
+from compound_explorer.compound_explorer_stack import CompoundExplorerStack, CompoundExplorerStackProps
+
+# Grab the account and region using boto3
+session = boto3.session.Session()
+aws_account = session.client("sts").get_caller_identity().get("Account")
+aws_region = session.region_name
+print(f"Account: {aws_account}")
+print(f"Region: {aws_region}")
+
+# When you want a different docker image change this line
+dashboard_image = "public.ecr.aws/m6i5k1r2/compound_explorer:v0_8_90_amd64"
+
+# Workbench Configuration (with some Explorer parameters)
+try:
+    from workbench.utils.config_manager import ConfigManager
+
+    cm = ConfigManager()
+    pprint(cm.config)
+    workbench_bucket = cm.get_config("WORKBENCH_BUCKET")
+    workbench_api_key = cm.get_config("WORKBENCH_API_KEY")
+    workbench_plugins = cm.get_config("WORKBENCH_PLUGINS")
+    existing_vpc_id = cm.get_config("EXPLORER_VPC_ID")
+    existing_subnet_ids = cm.get_config("EXPLORER_SUBNET_IDS")
+    config_ips = cm.get_config("EXPLORER_WHITELIST", "")
+    whitelist_ips = [ip.strip() for ip in config_ips.split(",") if ip.strip()]
+    config_prefix = cm.get_config("EXPLORER_PREFIX_LISTS", "")
+    whitelist_prefix_lists = [ip.strip() for ip in config_prefix.split(",") if ip.strip()]
+    certificate_arn = cm.get_config("EXPLORER_CERTIFICATE_ARN")
+except ImportError:
+    print("Workbench Configuration Manager Not Found...")
+    print("Set the WORKBENCH_CONFiG Env var and run again...")
+    raise SystemExit(1)
+
+
+# TODO: Add in a security group and pass as a prop to the stack
+
+app = cdk.App()
+CompoundExplorerStack(
+    app,
+    "CompoundExplorer",
+    env={"account": aws_account, "region": aws_region},
+    props=CompoundExplorerStackProps(
+        dashboard_image=dashboard_image,
+        workbench_bucket=workbench_bucket,
+        workbench_api_key=workbench_api_key,
+        workbench_plugins=workbench_plugins,
+        existing_vpc_id=existing_vpc_id,
+        existing_subnet_ids=existing_subnet_ids,
+        whitelist_ips=whitelist_ips,
+        whitelist_prefix_lists=whitelist_prefix_lists,
+        certificate_arn=certificate_arn,
+        public=True,
+    ),
+)
+
+app.synth()
