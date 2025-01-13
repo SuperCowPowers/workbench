a9d6b0b4461316354f5351bb1117efe6646b4cf0
Brian Wylie <briford.wylie@gmail.com>
2025-01-12 18:40:51 -0700

    comment change

--- src/workbench/utils/chem_utils.py
+++ src/workbench/utils/chem_utils.py
@@ -602,7 +602,7 @@ def project_fingerprints(df: pd.DataFrame, projection: str = "UMAP") -> pd.DataF
     if fingerprint_column is None:
         raise ValueError("Input DataFrame must have a fingerprint column")
 
-    # Convert the bitstring fingerprint into a NumPy array of integers
+    # Convert the bitstring fingerprint into a NumPy array of bools (bits)
     df["fingerprint_bits"] = df[fingerprint_column].apply(lambda fp: np.array([int(bit) for bit in fp], dtype=np.bool_))
 
     # Create a matrix of fingerprints
