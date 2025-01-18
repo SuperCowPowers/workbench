bbb971265a23e25d8345bfcab571b0f99d5655d4
Brian Wylie <briford.wylie@gmail.com>
2025-01-18 09:06:58 -0700

    testing the name arg

--- experiments/networkx_id.py
+++ experiments/networkx_id.py
@@ -13,10 +13,12 @@ for node, attrs in G.nodes(data=True):
     print(node, attrs)
 
 # Serialize to JSON
-graph_json = nx.readwrite.json_graph.node_link_data(G, edges="edges")
+graph_json = nx.readwrite.json_graph.node_link_data(G, edges="edges", name="dummy")
+# graph_json = nx.readwrite.json_graph.node_link_data(G, edges="edges")
 
 # Deserialize back
-G_deserialized = nx.readwrite.json_graph.node_link_graph(graph_json, edges="edges")
+G_deserialized = nx.readwrite.json_graph.node_link_graph(graph_json, edges="edges", name="dummy")
+# G_deserialized = nx.readwrite.json_graph.node_link_graph(graph_json, edges="edges")
 
 # Print nodes with attributes after deserialization
 print("\nAfter deserialization:")
