c267d461ba57eb64aae11b3c2951016797624673
Brian Wylie <briford.wylie@gmail.com>
2025-01-26 18:52:07 -0700

    moving CompoundDetails to GeneratedCompounds

--- src/workbench/web_interface/components/plugins/generated_compounds.py
+++ src/workbench/web_interface/components/plugins/generated_compounds.py
@@ -1,4 +1,4 @@
-"""A compound details plugin component"""
+"""A generated compounds plugin component"""
 
 from dash import html, dcc
 import dash_bootstrap_components as dbc
@@ -11,18 +11,22 @@ from workbench.utils.theme_manager import ThemeManager
 from workbench.utils.compound_utils import details_to_markdown
 from workbench.utils.ai_summary import AISummary
 from workbench.utils.ai_compound_generator import AICompoundGenerator
+from workbench.web_interface.components.plugins.molecule_viewer import MoleculeViewer
 
 
-class CompoundDetails(PluginInterface):
-    """Molecule Viewer Component"""
+class GeneratedCompounds(PluginInterface):
+    """Generated Compound Component"""
 
     # Initialize this Plugin Class with required attributes
     auto_load_page = PluginPage.NONE
     plugin_input_type = PluginInputType.COMPOUND
 
     def __init__(self):
-        """Initialize the CompoundDetails plugin class"""
+        """Initialize the GeneratedCompounds plugin class"""
         self.component_id = None
+
+        # Initialize the Molecule Viewer Plugin
+        self.molecule_viewer = MoleculeViewer()
 
         # Initialize the Theme Manager
         self.theme_manager = ThemeManager()
@@ -46,28 +50,19 @@ class CompoundDetails(PluginInterface):
         self.container = html.Div(
             id=self.component_id,
             children=[
-                # Left Column: Header + Molecular Image + Details + Summary
+                # Left Column: Molecule Viewer + Summary
                 dbc.Col(
                     [
-                        html.H3(id=f"{self.component_id}-header", children="Compound Details Loading..."),
-                        html.Img(
-                            id=f"{self.component_id}-img",
-                            className="workbench-container",
-                            style={"padding": "0px", "marginBottom": "30px"},
-                        ),
-                        dcc.Markdown(
-                            id=f"{self.component_id}-details",
-                            dangerously_allow_html=True,
-                            children="**Details Loading...**",
-                        ),
+                        self.molecule_viewer.create_component(f"{self.component_id}-molecule-viewer"),
                         dcc.Markdown(
                             id=f"{self.component_id}-summary",
                             dangerously_allow_html=True,
                             children="**Summary Details**",
+                            style={"margin-top": "20px"},
                         ),
                     ],
-                    style={"width": "480px", "flex": "0 0 auto"},
-                ),  # Fixed width of 480px
+                    style={"width": "480px", "flex": "0 0 auto", "marginRight": 30},  # Fixed width of 480px
+                ),
                 # Right Column: Generated Compounds
                 dbc.Col(
                     [
@@ -95,10 +90,8 @@ class CompoundDetails(PluginInterface):
         )
 
         # Fill in plugin properties
-        self.properties = [
-            (f"{self.component_id}-header", "children"),
-            (f"{self.component_id}-img", "src"),
-            (f"{self.component_id}-details", "children"),
+        self.properties = self.molecule_viewer.properties
+        self.properties += [
             (f"{self.component_id}-summary", "children"),
             (f"{self.component_id}-generative", "children"),
             (f"{self.component_id}-img-0", "src"),
@@ -129,11 +122,11 @@ class CompoundDetails(PluginInterface):
         # Header Text
         header_text = f"Compound: {compound.id}"
 
-        # Create the Molecule Image
-        img = compound.image(width, height)
+        # Hardcoded Smiles for now
+        compound.smiles = "CC(C)C1=CC=C(C=C1)C(=O)O"
 
-        # Compound Details
-        details = details_to_markdown(compound)
+        # Send the Compound to the Molecule Viewer
+        mol_view_props = self.molecule_viewer.update_properties(compound, width=450, height=300)
 
         # AI Summary for this compound
         ai_summary_markdown = self.ai_summary.smiles_query(compound.smiles)
@@ -151,7 +144,7 @@ class CompoundDetails(PluginInterface):
             images.append(img_src)
 
         # Return the updated property values for this plugin
-        return [header_text, img, details, ai_summary_markdown, ai_compound_markdown, *images]
+        return mol_view_props + [ai_summary_markdown, ai_compound_markdown, *images]
 
 
 if __name__ == "__main__":
@@ -159,9 +152,8 @@ if __name__ == "__main__":
     from workbench.web_interface.components.plugin_unit_test import PluginUnitTest
 
     # Run the Unit Test on the Plugin
-    # Run the Unit Test on the Plugin
     compound = Compound("AQSOL-0001")
     compound.smiles = "CC(C)C1=CC=C(C=C1)C(=O)O"
     compound.tags = ["toxic", "primary"]
     compound.meta = {"toxic_elements": None, "toxic_groups": ["[C;$(C#CH)]", "[C;$(C#CH)]"]}
-    PluginUnitTest(CompoundDetails, input_data=compound, theme="light").run()
+    PluginUnitTest(GeneratedCompounds, input_data=compound, theme="light").run()
