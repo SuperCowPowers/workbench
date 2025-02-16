7451bead53f7810d6dd63c3bd9ce4d2d55d55bcf
Brian Wylie <briford.wylie@gmail.com>
2025-02-15 18:55:24 -0700

    just looking at older views

--- scripts/admin/check_all_glue_tables.py
+++ scripts/admin/check_all_glue_tables.py
@@ -3,7 +3,7 @@ from workbench.api import Meta
 from pprint import pprint
 from datetime import datetime, timedelta, timezone
 
-older_than = datetime.now(timezone.utc) - timedelta(days=360)
+older_than = datetime.now(timezone.utc) - timedelta(days=90)
 
 
 def glue_table_inspection(database_scope):
