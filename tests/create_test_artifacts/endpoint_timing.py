ddf3ee8b4ce38cc1f520c88bbb9a0359d37804c6
Brian Wylie <briford.wylie@gmail.com>
2025-02-23 13:08:47 -0700

    switch to utils.fast_inference as a more realistic test

--- tests/create_test_artifacts/endpoint_timing.py
+++ tests/create_test_artifacts/endpoint_timing.py
@@ -4,6 +4,7 @@ import time
 
 # Workbench imports
 from workbench.api import FeatureSet, Endpoint
+from workbench.utils.fast_inference import fast_inference
 
 feature_set_name = "aqsol_features"
 id_column = "id"
@@ -14,19 +15,22 @@ df = fs.pull_dataframe()
 total_df = df[[id_column, "smiles"]]
 
 # First let's dump the information about all the endpoints
-end = Endpoint("tautomerize-v0-rt")
-print(f"Endpoint: {end.uuid}, Instance: {end.instance_type}")
-end = Endpoint("smiles-to-md-v0-rt")
-print(f"Endpoint: {end.uuid}, Instance: {end.instance_type}")
-end = Endpoint("aqsol-mol-class-rt")
-print(f"Endpoint: {end.uuid}, Instance: {end.instance_type}")
-end = Endpoint("pipeline-model")
-print(f"Endpoint: {end.uuid}, Instance: {end.instance_type}")
-end = Endpoint("pipeline-model-fast")
-print(f"Endpoint: {end.uuid}, Instance: {end.instance_type}")
+end_1 = Endpoint("tautomerize-v0-rt")
+print(f"Endpoint: {end_1.uuid}, Instance: {end_1.instance_type}")
+end_2 = Endpoint("smiles-to-md-v0-rt")
+print(f"Endpoint: {end_2.uuid}, Instance: {end_2.instance_type}")
+end_3 = Endpoint("aqsol-mol-class-rt")
+print(f"Endpoint: {end_3.uuid}, Instance: {end_3.instance_type}")
+end_pipe = Endpoint("pipeline-model")
+print(f"Endpoint: {end_pipe.uuid}, Instance: {end_pipe.instance_type}")
+end_pipe_fast = Endpoint("pipeline-model-fast")
+print(f"Endpoint: {end_pipe_fast.uuid}, Instance: {end_pipe_fast.instance_type}")
+
+# We're going to grab our Sagemaker Session from an endpoint (the all give the same session)
+session = end_1.sm_session
 
 # We're going to run 3 timings on 10, 100, 500, 1000, 10000 rows
-for n in [10, 100, 500, 1000, 10000]:
+for n in [10, 100, 500, 1000]:
     input_df = total_df.head(n)
     print(f"Timing for {n} rows")
 
@@ -34,20 +38,23 @@ for n in [10, 100, 500, 1000, 10000]:
 
     # Tautomerize
     total_start = time.time()
-    end_1 = Endpoint("tautomerize-v0-rt")
-    df = end_1.fast_inference(input_df)
+    # end_1 = Endpoint("tautomerize-v0-rt")
+    # df = end_1.fast_inference(input_df)
+    df = fast_inference(end_1.uuid, input_df, session)
     time_taut = time.time() - total_start
 
     # Molecular Descriptors
     start = time.time()
-    end_2 = Endpoint("smiles-to-md-v0-rt")
-    df = end_2.fast_inference(df)
+    # end_2 = Endpoint("smiles-to-md-v0-rt")
+    # df = end_2.fast_inference(df)
+    df = fast_inference(end_2.uuid, df, session)
     time_md = time.time() - start
 
     # AQSOL Classification
     start = time.time()
-    end_3 = Endpoint("aqsol-mol-class-rt")
-    df = end_3.fast_inference(df)
+    # end_3 = Endpoint("aqsol-mol-class-rt")
+    # df = end_3.fast_inference(df)
+    df = fast_inference(end_3.uuid, df, session)
     time_model = time.time() - start
     total_time = time.time() - total_start
 
@@ -56,14 +63,16 @@ for n in [10, 100, 500, 1000, 10000]:
 
     # Now time the pipeline endpoint
     start = time.time()
-    end = Endpoint("pipeline-model")
-    df = end.fast_inference(input_df)
+    # end_pipe = Endpoint("pipeline-model")
+    # df = end_pipe.fast_inference(input_df)
+    df = fast_inference(end_pipe.uuid, input_df, session)
     end = time.time()
     print(f"Pipeline endpoint: {end - start} seconds")
 
     # Now time the pipeline fast endpoint
     start = time.time()
-    end = Endpoint("pipeline-model-fast")
-    df = end.fast_inference(input_df)
+    # end_pipe_fast = Endpoint("pipeline-model-fast")
+    # df = end_pipe_fast.fast_inference(input_df)
+    df = fast_inference(end_pipe_fast.uuid, input_df, session)
     end = time.time()
     print(f"Pipeline Fast endpoint: {end - start} seconds")
