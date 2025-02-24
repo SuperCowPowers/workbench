6792c99c756ef081c082410cf04f88b175762e73
Brian Wylie <briford.wylie@gmail.com>
2025-02-24 07:58:56 -0700

    put a main around the pipeline model script so that pytest doesn't try to run it

--- tests/create_test_artifacts/create_pipeline_model.py
+++ tests/create_test_artifacts/create_pipeline_model.py
@@ -4,26 +4,28 @@ from sagemaker.pipeline import PipelineModel
 from workbench.api import Model
 from workbench.core.cloud_platform.aws.aws_account_clamp import AWSAccountClamp
 
-session = AWSAccountClamp().sagemaker_session()
-role = AWSAccountClamp().aws_session.get_workbench_execution_role_arn()
 
-# Our three models
-taut_model = Model("tautomerize-v0").sagemaker_model_object()
-md_model = Model("smiles-to-md-v0").sagemaker_model_object()
-sol_model = Model("aqsol-mol-class").sagemaker_model_object()
+if __name__ == "__main__":
+    session = AWSAccountClamp().sagemaker_session()
+    role = AWSAccountClamp().aws_session.get_workbench_execution_role_arn()
 
-# Create a pipeline model that chains the three models
-pipeline_model = PipelineModel(
-    name="pipeline-model", models=[taut_model, md_model, sol_model], role=role, sagemaker_session=session
-)
+    # Our three models
+    taut_model = Model("tautomerize-v0").sagemaker_model_object()
+    md_model = Model("smiles-to-md-v0").sagemaker_model_object()
+    sol_model = Model("aqsol-mol-class").sagemaker_model_object()
 
-# Deploy the pipeline endpoint
-predictor = pipeline_model.deploy(initial_instance_count=1, instance_type="ml.t2.medium")
+    # Create a pipeline model that chains the three models
+    pipeline_model = PipelineModel(
+        name="pipeline-model", models=[taut_model, md_model, sol_model], role=role, sagemaker_session=session
+    )
 
-# Create a pipeline model with slightly better endpoint instance
-pipeline_model = PipelineModel(
-    name="pipeline-model-fast", models=[taut_model, md_model, sol_model], role=role, sagemaker_session=session
-)
+    # Deploy the pipeline endpoint
+    predictor = pipeline_model.deploy(initial_instance_count=1, instance_type="ml.t2.medium")
 
-# Deploy the pipeline endpoint
-predictor = pipeline_model.deploy(initial_instance_count=1, instance_type="ml.c7i.xlarge")
+    # Create a pipeline model with slightly better endpoint instance
+    pipeline_model = PipelineModel(
+        name="pipeline-model-fast", models=[taut_model, md_model, sol_model], role=role, sagemaker_session=session
+    )
+
+    # Deploy the pipeline endpoint
+    predictor = pipeline_model.deploy(initial_instance_count=1, instance_type="ml.c7i.xlarge")
