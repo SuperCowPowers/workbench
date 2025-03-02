7b44ec883aa6e0ad1b40e2c95db3d56fc3f3bce6
Brian Wylie <briford.wylie@gmail.com>
2025-03-02 13:43:16 -0700

    fix test

--- tests/specific/capital_tests.py
+++ tests/specific/capital_tests.py
@@ -6,8 +6,7 @@ from pprint import pprint
 @pytest.mark.long
 def test():
     # Create a new Data Source from an S3 Path (or a local file)
-    source_path = "s3://workbench-public-data/common/aBaLone.CSV"
-    # source_path = "/full/path/to/local/file.csv"
+    source_path = "s3://workbench-public-data/common/abalone.csv"
     my_data = DataSource(source_path)
     pprint(my_data.summary())
     pprint(my_data.details())
