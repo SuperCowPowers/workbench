15a7ae86615a531e73af1118ae47ae1fa2232de9
Brian Wylie <briford.wylie@gmail.com>
2025-03-08 12:21:04 -0700

    flake8/linter cleanup

--- scripts/seaborn_to_plotly.py
+++ scripts/seaborn_to_plotly.py
@@ -7,7 +7,7 @@ vals = np.linspace(0, 1, 8)
 plotly_scale = []
 for v in vals:
     r, g, b, a = cmap(v)
-    plotly_scale.append([round(v, 2), f'rgba({int(r*255)}, {int(g*255)}, {int(b*255)}, {a})'])
+    plotly_scale.append([round(v, 2), f"rgba({int(r*255)}, {int(g*255)}, {int(b*255)}, {a})"])
 
 for sublist in plotly_scale:
     print(json.dumps(sublist))
