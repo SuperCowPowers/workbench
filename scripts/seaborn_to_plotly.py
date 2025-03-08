505fc6c6a44ed26d85fba4ecf2e18dd6adad0f76
Brian Wylie <briford.wylie@gmail.com>
2025-03-08 12:20:15 -0700

    adding a helper script for seaborn to plotly colorscales

---
+++ scripts/seaborn_to_plotly.py
@@ -0,0 +1,13 @@
+import numpy as np
+import seaborn as sns
+import json
+
+cmap = sns.light_palette("#97c3d5", as_cmap=True)
+vals = np.linspace(0, 1, 8)
+plotly_scale = []
+for v in vals:
+    r, g, b, a = cmap(v)
+    plotly_scale.append([round(v, 2), f'rgba({int(r*255)}, {int(g*255)}, {int(b*255)}, {a})'])
+
+for sublist in plotly_scale:
+    print(json.dumps(sublist))
