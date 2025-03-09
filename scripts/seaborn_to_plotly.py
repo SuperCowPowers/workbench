0f8c5a876b3b438fb03ae867c26ed6141b8227d9
Brian Wylie <briford.wylie@gmail.com>
2025-03-08 19:54:20 -0700

    update helper script

--- scripts/seaborn_to_plotly.py
+++ scripts/seaborn_to_plotly.py
@@ -2,7 +2,10 @@ import numpy as np
 import seaborn as sns
 import json
 
-cmap = sns.light_palette("#97c3d5", as_cmap=True)
+cmap = sns.light_palette("#97c3d5", as_cmap=True)  # Light Blue
+cmap = sns.light_palette("#175676", as_cmap=True)  # Dark Blue
+# cmap = sns.light_palette("#F46036",as_cmap=True) #orange
+# cmap = sns.light_palette("#8dc9b7",as_cmap=True) #green
 vals = np.linspace(0, 1, 8)
 plotly_scale = []
 for v in vals:
