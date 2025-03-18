07297581f57ca1735e0337d7c043b4313428231a
Brian Wylie <briford.wylie@gmail.com>
2025-03-18 10:04:26 -0600

    adding ProximityType to the __init__

--- src/workbench/algorithms/dataframe/__init__.py
+++ src/workbench/algorithms/dataframe/__init__.py
@@ -5,13 +5,14 @@ These classes provide functionality for Pandas Dataframes
 - TBD: TBD
 """
 
-from .proximity import Proximity
+from .proximity import Proximity, ProximityType
 from .feature_space_proximity import FeatureSpaceProximity
 from .fingerprint_proximity import FingerprintProximity
 from .projection_2d import Projection2D
 
 __all__ = [
     "Proximity",
+    "ProximityType",
     "FeatureSpaceProximity",
     "FingerprintProximity",
     "Projection2D",
