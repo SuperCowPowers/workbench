bb8036ec37974f05cf9428de47706b10fa6c8610
Brian Wylie <briford.wylie@gmail.com>
2025-04-14 07:40:14 -0600

    adding a test for endpoint chaining

---
+++ tests/corner_cases/end_to_end.py
@@ -0,0 +1,48 @@
+"""Tests for Endpoint 'chaining''"""
+
+import pandas as pd
+
+# Workbench Imports
+from workbench.api import Endpoint
+
+end_reg = Endpoint("abalone-regression")
+end_class = Endpoint("wine-classification")
+
+
+def test_reg_to_reg():
+    """Test chaining two regression endpoints"""
+    pred_df = end_reg.inference(end_reg.auto_inference())
+    print(pred_df.columns)
+    print(pred_df)
+
+
+def test_reg_to_class():
+    """Test chaining a regression endpoint to a classification endpoint"""
+    pred_df = end_class.inference(end_reg.auto_inference())
+    print(pred_df.columns)
+    print(pred_df)
+
+
+def test_class_to_class():
+    """Test chaining two classification endpoints"""
+    pred_df = end_class.inference(end_class.auto_inference())
+    print(pred_df.columns)
+    print(pred_df)
+
+
+def test_class_to_reg():
+    """Test chaining a classification endpoint to a regression endpoint"""
+    pred_df = end_reg.inference(end_class.auto_inference())
+    print(pred_df.columns)
+    print(pred_df)
+
+
+if __name__ == "__main__":
+
+    # Run the tests
+    test_reg_to_reg()
+    # test_reg_to_class()
+    test_class_to_class()
+    # test_class_to_reg()
+
+    print("All tests passed!")
