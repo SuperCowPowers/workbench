a0f35307c64f7eb395bef373a1a844b9ff62e2cf
Brian Wylie <briford.wylie@gmail.com>
2025-04-14 12:08:28 -0600

    small fix for no line after health tags

--- src/workbench/utils/markdown_utils.py
+++ src/workbench/utils/markdown_utils.py
@@ -26,6 +26,7 @@ def health_tag_markdown(health_tags: list[str]) -> str:
 
     # If we have health tags, then add a bullet for each tag
     markdown += "\n".join(f"* {tag}: {health_icons.get(tag, '')}" for tag in health_tags)
+    markdown += "\n\n"
     return markdown
 
 
