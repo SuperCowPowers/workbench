439fe409b0a73d6beba4519a443e5650be2bd266
Brian Wylie <briford.wylie@gmail.com>
2025-04-15 15:44:34 -0600

    slimming down the example meta endpoint

--- src/workbench/model_scripts/custom_models/meta_endpoints/example.py
+++ src/workbench/model_scripts/custom_models/meta_endpoints/example.py
@@ -1,8 +1,5 @@
 # Model: Meta Endpoint Example
-#
-import argparse
-import os
-import joblib
+# This script is a template for creating a custom meta endpoint in AWS Workbench.
 from io import StringIO
 import pandas as pd
 import json
@@ -14,25 +11,9 @@ except ImportError:
     print("workbench_bridges not found, this is fine for training...")
 
 
-# TRAINING SECTION (not used)
-if __name__ == "__main__":
-    # Script arguments for input/output directories
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model-dir", type=str, default=os.environ.get("SM_MODEL_DIR", "/opt/ml/model"))
-    parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN", "/opt/ml/input/data/train"))
-    parser.add_argument(
-        "--output-data-dir", type=str, default=os.environ.get("SM_OUTPUT_DATA_DIR", "/opt/ml/output/data")
-    )
-    args = parser.parse_args()
-
-    # Sagemaker seems to get upset if we don't save a model, so we'll create a placeholder model
-    placeholder_model = {}
-    joblib.dump(placeholder_model, os.path.join(args.model_dir, "model.joblib"))
-
-
-# Model loading and prediction functions
+# Not Used: We need to define this function for SageMaker
 def model_fn(model_dir):
-    return joblib.load(os.path.join(model_dir, "model.joblib"))
+    return None
 
 
 def input_fn(input_data, content_type):
