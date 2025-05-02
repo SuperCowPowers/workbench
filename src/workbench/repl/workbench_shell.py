a4181962456007e8bb375a89c4a7dea2135b57d3
Brian Wylie <briford.wylie@gmail.com>
2025-05-02 09:10:59 -0600

    adding secrets import to repl

--- src/workbench/repl/workbench_shell.py
+++ src/workbench/repl/workbench_shell.py
@@ -156,6 +156,7 @@ class WorkbenchShell:
         self.commands["log"] = logging.getLogger("workbench")
         self.commands["get_meta"] = self.get_meta
         self.commands["params"] = importlib.import_module("workbench.api.parameter_store").ParameterStore()
+        self.commands["secrets"] = importlib.import_module("workbench.core.cloud_platform.aws.aws_secrets_manager").AWSSecretsManager()
         self.commands["df_store"] = importlib.import_module("workbench.api.df_store").DFStore()
         self.commands["graph_store"] = importlib.import_module("workbench.api.graph_store").GraphStore()
         self.commands["version"] = lambda: print(version)
