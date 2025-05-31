e6e2e6d999959e62b4059f213b07f5c5f5a016bb
Brian Wylie <briford.wylie@gmail.com>
2025-05-31 17:08:49 -0600

    adding ngboost to the confidence models

--- examples/featureset_to_model/featureset_to_ngboost.py
+++ examples/featureset_to_model/featureset_to_ngboost.py
@@ -10,54 +10,29 @@ my_features = FeatureSet("aqsol_features")
 model = Model("aqsol-regression")
 
 # Get the custom script path for the proximity model
-script_path = get_custom_script_path("confidence", "confidence.template")
+script_path = get_custom_script_path("confidence", "ngboost.template")
 
 # Get Feature and Target Columns from the existing given Model
 features = model.features()
 target = model.target()
 
-"""
-# Create the BayesianRidge Model from our FeatureSet
+# Create the NGBoost Model from our FeatureSet
 fs = FeatureSet(model.get_input())
 my_model = fs.to_model(
-    name="aqsol-bayesian-reg",
+    name="aqsol-ngboost-reg",
     model_type=ModelType.REGRESSOR,
-    model_class="BayesianRidge",
     feature_list=features,
     target_column=target,
-    description=f"Bayesian Ridge Model for AQSol",
-    tags=["aqsol", "bayesian", "regression"],
+    description=f"NGBoost Model for AQSol",
+    tags=["aqsol", "ngboost", "regression"],
     custom_script=script_path,
 )
 # Print the details of the created model
 pprint(my_model.details())
-my_model = Model("aqsol-bayesian-reg")
+my_model = Model("aqsol-ngboost-reg")
 
 # Deploy an Endpoint for the Model
-endpoint = my_model.to_endpoint(tags=["aqsol", "bayesian"])
-
-# Run auto-inference on the Endpoint
-endpoint.auto_inference(capture=True)
-"""
-
-# Create the GaussianProcess Model from our FeatureSet
-fs = FeatureSet(model.get_input())
-my_model = fs.to_model(
-    name="aqsol-gaussian-reg",
-    model_type=ModelType.REGRESSOR,
-    model_class="GaussianProcessRegressor",
-    feature_list=features,
-    target_column=target,
-    description=f"GaussianProcessRegressor Model for AQSol",
-    tags=["aqsol", "gaussian", "regression"],
-    custom_script=script_path,
-)
-# Print the details of the created model
-pprint(my_model.details())
-my_model = Model("aqsol-gaussian-reg")
-
-# Deploy an Endpoint for the Model
-endpoint = my_model.to_endpoint(tags=["aqsol", "gaussian"])
+endpoint = my_model.to_endpoint(tags=["aqsol", "ngboost"])
 
 # Run auto-inference on the Endpoint
 endpoint.auto_inference(capture=True)
