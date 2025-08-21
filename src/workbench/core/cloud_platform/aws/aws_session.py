92cb53a7a1530fed34a6e0f04d3adec8db1a1661
Brian Wylie <briford.wylie@gmail.com>
2025-08-21 17:10:23 -0600

    fixing an errant change, going back to c_print for now

--- src/workbench/core/cloud_platform/aws/aws_session.py
+++ src/workbench/core/cloud_platform/aws/aws_session.py
@@ -149,7 +149,7 @@ class AWSSession:
             return credentials
 
         except Exception as e:
-            self.log.critical(f"Error during Refresh Credentials: {e}", critical=True)
+            self.c_print(f"Error during Refresh Credentials: {e}", critical=True)
             raise RuntimeError("Failed to refresh Workbench role session credentials")
 
     @staticmethod
