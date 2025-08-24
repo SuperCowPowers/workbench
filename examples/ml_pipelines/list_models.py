edecccd0c88eff3b1b45bfc606997f073da4be45
Brian Wylie <briford.wylie@gmail.com>
2025-08-24 14:47:40 -0600

    adding an ml_pipeline example

---
+++ examples/ml_pipelines/list_models.py
@@ -0,0 +1,14 @@
+import time
+import logging
+from workbench.api import Meta
+
+log = logging.getLogger("workbench")
+
+# List all the models in AWS
+meta = Meta()
+models = meta.models()
+log.info(f"Found {len(models)} models in AWS")
+log.info(models)
+
+# Test out the ml launcher disconnect
+time.sleep(120)
