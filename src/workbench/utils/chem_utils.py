f9421a139e260c6709632ff011ec8d2be6bca8a7
Brian Wylie <briford.wylie@gmail.com>
2025-09-05 16:38:09 -0600

    flake8/linter fixes

--- src/workbench/utils/chem_utils.py
+++ src/workbench/utils/chem_utils.py
@@ -1559,21 +1559,21 @@ if __name__ == "__main__":
     test_data = {
         "id": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         "smiles": [
-            '[O-]C([O-])=O.[Na+].[Na+]',
-            '[Pr+3].[Pr+3].[O-]C([O-])=O.[O-]C([O-])=O.[O-]C([O-])=O',
-            '[O-]C([O-])=O.[Na+].[Na+]',
-            '[O-]C([O-])=O.[Li+].[Li+]',
-            '[Ce+3].[Ce+3].[O-]C([O-])=O.[O-]C([O-])=O.[O-]C([O-])=O',
-            '[Co++].[O-]C([O-])=O',
-            '[Mn++].[O-]C([O-])=O',
-            '[Cs+].[Cs+].[O-]C([O-])=O',
-            '[O-]C([O-])=O.[Cd++]',
-            '[O-]C([O-])=O.[NH4+].[NH4+]',
-            '[O-]C([O-])=O.[Zn++].[NH4+]',
-            '[O-]C([O-])=O.[Na+].[Na+]',
-            '[Mg++].[O-]C([O-])=O',
-            '[Nd+3].[Nd+3].[O-]C([O-])=O.[O-]C([O-])=O.[O-]C([O-])=O',
-            '[O-]C([O-])=O.[K+].[K+]'
+            "[O-]C([O-])=O.[Na+].[Na+]",
+            "[Pr+3].[Pr+3].[O-]C([O-])=O.[O-]C([O-])=O.[O-]C([O-])=O",
+            "[O-]C([O-])=O.[Na+].[Na+]",
+            "[O-]C([O-])=O.[Li+].[Li+]",
+            "[Ce+3].[Ce+3].[O-]C([O-])=O.[O-]C([O-])=O.[O-]C([O-])=O",
+            "[Co++].[O-]C([O-])=O",
+            "[Mn++].[O-]C([O-])=O",
+            "[Cs+].[Cs+].[O-]C([O-])=O",
+            "[O-]C([O-])=O.[Cd++]",
+            "[O-]C([O-])=O.[NH4+].[NH4+]",
+            "[O-]C([O-])=O.[Zn++].[NH4+]",
+            "[O-]C([O-])=O.[Na+].[Na+]",
+            "[Mg++].[O-]C([O-])=O",
+            "[Nd+3].[Nd+3].[O-]C([O-])=O.[O-]C([O-])=O.[O-]C([O-])=O",
+            "[O-]C([O-])=O.[K+].[K+]",
         ],
     }
     df = pd.DataFrame(test_data)
@@ -1581,4 +1581,3 @@ if __name__ == "__main__":
     # Compute Molecular Descriptors
     df = compute_molecular_descriptors(df)
     print(df)
-
