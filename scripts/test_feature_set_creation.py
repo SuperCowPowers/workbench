8298e379b4e110d18e177235f570512658de07c4
Brian Wylie <briford.wylie@gmail.com>
2025-10-16 10:17:57 -0600

    adding a script for testing feature set creation issues

---
+++ scripts/test_feature_set_creation.py
@@ -0,0 +1,13 @@
+import awswrangler as wr
+
+# Workbench Imports
+from workbench.core.transforms.pandas_transforms import PandasToFeatures
+
+s3_path = "s3://workbench-public-data/comp_chem/aqsol_public_data.csv"
+aqsol_data = wr.s3.read_csv(s3_path)
+aqsol_data.columns = aqsol_data.columns.str.lower()
+
+to_features = PandasToFeatures("temp_delete_me")
+to_features.set_input(aqsol_data, id_column="id")
+to_features.set_output_tags(["aqsol", "public"])
+to_features.transform()
