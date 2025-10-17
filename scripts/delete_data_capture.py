c38adbaafe326d885ea88c11b9d41249954879b1
Brian Wylie <briford.wylie@gmail.com>
2025-10-17 07:58:01 -0600

    flake8/linter fixes

--- scripts/delete_data_capture.py
+++ scripts/delete_data_capture.py
@@ -8,33 +8,33 @@ from workbench.utils.config_manager import ConfigManager
 
 
 def list_and_delete_data_capture(bucket, preview=True):
-    s3 = boto3.client('s3')
-    prefix = 'endpoints/'
-    
+    s3 = boto3.client("s3")
+    prefix = "endpoints/"
+
     # Get all endpoint prefixes
-    paginator = s3.get_paginator('list_objects_v2')
+    paginator = s3.get_paginator("list_objects_v2")
     endpoints = set()
-    
-    for page in paginator.paginate(Bucket=bucket, Prefix=prefix, Delimiter='/'):
-        for common_prefix in page.get('CommonPrefixes', []):
-            endpoint_prefix = common_prefix['Prefix']
+
+    for page in paginator.paginate(Bucket=bucket, Prefix=prefix, Delimiter="/"):
+        for common_prefix in page.get("CommonPrefixes", []):
+            endpoint_prefix = common_prefix["Prefix"]
             if endpoint_prefix != prefix:
                 endpoints.add(endpoint_prefix)
-    
+
     # Count and optionally delete files per endpoint
     counts = defaultdict(int)
     to_delete = []
-    
+
     for endpoint_prefix in endpoints:
         data_capture_prefix = f"{endpoint_prefix}data_capture/"
-        
+
         for page in paginator.paginate(Bucket=bucket, Prefix=data_capture_prefix):
-            for obj in page.get('Contents', []):
-                if obj['Key'].endswith('.jsonl'):
-                    endpoint_name = endpoint_prefix.split('/')[-2]
+            for obj in page.get("Contents", []):
+                if obj["Key"].endswith(".jsonl"):
+                    endpoint_name = endpoint_prefix.split("/")[-2]
                     counts[endpoint_name] += 1
-                    to_delete.append(obj['Key'])
-    
+                    to_delete.append(obj["Key"])
+
     # Display results
     if counts:
         print(f"\n{'Endpoint':<50} {'Files'}")
@@ -46,30 +46,27 @@ def list_and_delete_data_capture(bucket, preview=True):
     else:
         print("No .jsonl files found in data_capture directories")
         return
-    
+
     # Delete if not preview mode
     if not preview:
         print("Deleting files...")
         # Delete in batches of 1000
         for i in range(0, len(to_delete), 1000):
-            batch = to_delete[i:i+1000]
-            s3.delete_objects(
-                Bucket=bucket,
-                Delete={'Objects': [{'Key': k} for k in batch]}
-            )
+            batch = to_delete[i : i + 1000]
+            s3.delete_objects(Bucket=bucket, Delete={"Objects": [{"Key": k} for k in batch]})
         print(f"Deleted {len(to_delete)} files")
     else:
         print("PREVIEW MODE - no files deleted")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Remove data capture files from SageMaker endpoints')
-    parser.add_argument('--delete', action='store_true', help='Actually delete files (default is preview)')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Remove data capture files from SageMaker endpoints")
+    parser.add_argument("--delete", action="store_true", help="Actually delete files (default is preview)")
     args = parser.parse_args()
 
     # Get the default S3 bucket from Workbench config
     config = ConfigManager()
     bucket = config.get_config("WORKBENCH_BUCKET")
     print(f"Using S3 bucket: {bucket}")
-    
+
     list_and_delete_data_capture(bucket, preview=not args.delete)
