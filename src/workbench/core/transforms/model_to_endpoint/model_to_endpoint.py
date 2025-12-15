bf74b5b8e0e34bbc83fad8dc4e4776cb4449bbc2
Brian Wylie <briford.wylie@gmail.com>
2025-12-15 11:14:52 -0700

    putting in a check for at least 4GB of memory for PyTorch and ChemProp

--- src/workbench/core/transforms/model_to_endpoint/model_to_endpoint.py
+++ src/workbench/core/transforms/model_to_endpoint/model_to_endpoint.py
@@ -102,6 +102,12 @@ class ModelToEndpoint(Transform):
         # Is this a serverless deployment?
         serverless_config = None
         if self.serverless:
+            # For PyTorch or ChemProp we need at least 4GB of memory
+            from workbench.api import ModelFramework
+            if workbench_model.model_framework in [ModelFramework.PYTORCH_TABULAR, ModelFramework.CHEMPROP]:
+                if mem_size < 4096:
+                    self.log.important(f"{workbench_model.model_framework} needs at least 4GB of memory (setting to 4GB)")
+                    mem_size = 4096
             serverless_config = ServerlessInferenceConfig(
                 memory_size_in_mb=mem_size,
                 max_concurrency=max_concurrency,
