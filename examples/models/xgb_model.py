d87ba1d435e60ada33868ab1649f196387b539f8
Brian Wylie <briford.wylie@gmail.com>
2025-12-16 20:04:55 -0700

    adding an xgboost model example script

---
+++ examples/models/xgb_model.py
@@ -0,0 +1,81 @@
+from workbench.api import FeatureSet, Model, ModelType, Endpoint
+
+# Grab a FeatureSet
+my_features = FeatureSet("aqsol_features")
+model = Model("aqsol-regression")
+feature_list = [
+    "molwt",
+    "mollogp",
+    "molmr",
+    "heavyatomcount",
+    "numhacceptors",
+    "numhdonors",
+    "numheteroatoms",
+    "numrotatablebonds",
+    "numvalenceelectrons",
+    "numaromaticrings",
+    "numsaturatedrings",
+    "numaliphaticrings",
+    "ringcount",
+    "tpsa",
+    "labuteasa",
+    "balabanj",
+    "bertzct",
+]
+target = "solubility"
+
+# Recreate Flag in case you want to recreate the artifacts
+recreate = True
+
+# PyTorch Regression Model
+if recreate or not Model("aqsol-regression").exists():
+    feature_set = FeatureSet("aqsol_features")
+    m = feature_set.to_model(
+        name="aqsol-regression",
+        model_type=ModelType.UQ_REGRESSOR,
+        feature_list=feature_list,
+        target_column=target,
+        description="PyTorch Regression Model for AQSol",
+        tags=["pytorch", "molecular descriptors"],
+        hyperparameters={
+            "training_config": {"max_epochs": 150},
+            "model_config": {"layers": "128-64-32"},
+        },
+    )
+    m.set_owner("BW")
+
+# Create an Endpoint for the Regression Model
+if recreate or not Endpoint("aqsol-regression").exists():
+    m = Model("aqsol-regression")
+    end = m.to_endpoint(tags=["pytorch", "molecular descriptors"])
+    end.set_owner("BW")
+
+    # Run inference on the endpoint
+    end.auto_inference(capture=True)
+
+# Pytorch Classification Model
+if recreate or not Model("aqsol-class").exists():
+    feature_set = FeatureSet("aqsol_features")
+    m = feature_set.to_model(
+        name="aqsol-class",
+        model_type=ModelType.CLASSIFIER,
+        feature_list=feature_list,
+        target_column="solubility_class",
+        description="PyTorch Classification Model for AQSol",
+        tags=["pytorch", "molecular descriptors"],
+        hyperparameters={
+            "training_config": {"max_epochs": 150},
+            "model_config": {"layers": "256-128-64"},
+        },
+    )
+    m.set_owner("BW")
+    m.set_class_labels(["low", "medium", "high"])
+
+# Create an Endpoint for the Classification Model
+if recreate or not Endpoint("aqsol-class").exists():
+    m = Model("aqsol-class")
+    end = m.to_endpoint(tags=["pytorch", "molecular descriptors"])
+    end.set_owner("BW")
+
+    # Run inference on the endpoint
+    end.auto_inference(capture=True)
