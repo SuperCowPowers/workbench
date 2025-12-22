18ac6ee688037829e4a8e64e0bb4b94111a93094
Brian Wylie <briford.wylie@gmail.com>
2025-12-22 11:37:59 -0700

    removing the old feature space proximity class

--- src/workbench/algorithms/dataframe/__init__.py
+++ src/workbench/algorithms/dataframe/__init__.py
@@ -6,13 +6,11 @@ These classes provide functionality for Pandas Dataframes
 """
 
 from .proximity import Proximity
-from .feature_space_proximity import FeatureSpaceProximity
 from .fingerprint_proximity import FingerprintProximity
 from .projection_2d import Projection2D
 
 __all__ = [
     "Proximity",
-    "FeatureSpaceProximity",
     "FingerprintProximity",
     "Projection2D",
 ]
