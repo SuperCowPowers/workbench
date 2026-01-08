0509fe88ae679f82df54fc960fa9877a5ed996fb
Brian Wylie <briford.wylie@gmail.com>
2026-01-08 15:23:05 -0700

    adding smiles to fingerprint example script

---
+++ examples/models/smiles_to_fingerprints.py
@@ -0,0 +1,25 @@
+"""Create a Smiles to Fingerprint Model/Endpoint"""
+
+# Workbench Imports
+from workbench.api import FeatureSet, ModelType
+from workbench.utils.model_utils import get_custom_script_path
+
+fs_name = "aqsol_features"
+
+# A 'Model' to Compute Morgan Fingerprints Features
+script_path = get_custom_script_path("chem_info", "morgan_fingerprints.py")
+feature_set = FeatureSet("aqsol_features")
+model = feature_set.to_model(
+    name="smiles-to-fingerprints-v0",
+    model_type=ModelType.TRANSFORMER,
+    feature_list=["smiles"],
+    description="Smiles to Morgan Fingerprints",
+    tags=["smiles", "morgan fingerprints"],
+    custom_script=script_path,
+)
+
+# Create the endpoint for the model
+end = model.to_endpoint(tags=["smiles", "morgan fingerprints"])
+
+# Run inference on the endpoint
+end.auto_inference()
