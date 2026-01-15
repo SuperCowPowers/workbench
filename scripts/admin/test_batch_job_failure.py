730c68d31d4607b122ca912c1284af1f0668e207
Brian Wylie <briford.wylie@gmail.com>
2026-01-15 13:28:41 -0700

    adding an ml_pipeline/batch job that crashes on purpose

---
+++ scripts/admin/test_batch_job_failure.py
@@ -0,0 +1,10 @@
+from workbench.api import FeatureSet, Model, ModelType, Endpoint
+
+# Grab a FeatureSet
+my_features = FeatureSet("aqsol_features")
+model = Model("aqsol-regression")
+end = Endpoint("aqsol-regression")
+
+# Now give the endpoint an incorrect method on purpose to test batch job failure handling
+end.non_existent_method()
+print("Batch job failure test completed.")
