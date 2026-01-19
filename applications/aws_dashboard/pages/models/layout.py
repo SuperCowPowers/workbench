11a35516493631831de3bcf7c9194bbde22aba96
Brian Wylie <briford.wylie@gmail.com>
2026-01-19 08:37:58 -0700

    making the model details a bit thinner

--- applications/aws_dashboard/pages/models/layout.py
+++ applications/aws_dashboard/pages/models/layout.py
@@ -38,7 +38,7 @@ def models_layout(
                     # Column 1: Model Details
                     dbc.Col(
                         dcc.Loading(model_details, type="dot", color="#33aa33", delay_show=1000, delay_hide=100),
-                        width=5,
+                        width=4,
                         className="text-break workbench-container",
                         style={"margin": "20px 0px 0px 0px", "padding": "20px"},
                     ),
